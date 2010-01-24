bf0e82cf2ac02a652e58269b223eaf4ee4552082
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-01-24 23:26:00 +0200

    Added some obfuscated cmd.exe scripting

---
+++ old/Just another cmd.exe hacker.cmd
@@ -0,0 +1,44 @@
+@echo off & setlocal || localset | off echo@
+set equ=unless
+set if= "
+
+>nul,set^
+ echo=Call
+>nul,^set
+ echo=Just
+
+ goto :neq
+                       :equ goto
+echo   cmd.exe || cmd.exe   echo
+ goto :eof
+                       :neq call
+
+,goto,goto,
+
+:echo.
+^goto.another.^
+
+:&:
+
+:goto:goto
+  ^echo, another &goto^
+    echo.
+      ^ another echo.
+        goto:goto:
+
+Isn't it fun? ^.^
+
+:.another.:
+
+
+if%if%if%if% equ %if%if%if% call, :equ
+
+goto:call, ^
+call:911, ^
+echo: And a call call here, and a call call there ^
+:EIEIO
+
+::Use the "call :label" command to
+:call a "subroutine" that returns upon reaching EOF.
+echo, ^
+  ^ hacker,

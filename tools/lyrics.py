e8e9401d98f5bb30a186712771f9bbb0eb9d94ea
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-04-07 23:21:39 +0300

    Use msvcrt.setmode() to force binary stdio

--- tools/lyrics.py
+++ tools/lyrics.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 # Read and write lyrics tags.
 import sys
 import getopt
+
+# Turn off text mode stdio on Windows (otherwise it writes CR CR LF)
+if sys.platform == "win32":
+	import os, msvcrt
+	msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+
 try:
-	import mutagen
+	import mutagen.mp3, mutagen.id3
 except ImportError:
 	print >> sys.stderr, "The mutagen library is not installed."
 	sys.exit(42)
@@ -28,16 +34,14 @@ def read_id3(file):
 	except KeyError:
 		return None
 
-shortopts = "i" "o" "x" "f:"
-longopts = []
+mode = "output"
+lyricsfile = None
+
 try:
-	options, files = getopt.gnu_getopt(sys.argv[1:], shortopts, longopts)
+	options, files = getopt.gnu_getopt(sys.argv[1:], "f:iox")
 except getopt.GetoptError as e:
 	print >> sys.stderr, "Error:", e
 	sys.exit(2)
-
-mode = "output"
-lyricsfile = None
 
 for opt, value in options:
 	if   opt == "-i": mode = "input"
@@ -46,27 +50,32 @@ for opt, value in options:
 	elif opt == "-f": lyricsfile = value
 
 if mode == "input":
-	# input: write lyrics into file
-	if lyricsfile is not None:
-		sys.stdin = open(lyricsfile, "r")
-	lyrics = sys.stdin.read().decode("utf-8")
-	
+	if lyricsfile is none:
+		f = sys.stdin
+	else:
+		f = open(lyricsfile, "r")
+
+	lyrics = f.read().decode("utf-8")
+
 	for file in files:
 		write_id3(file, lyrics)
 		
 elif mode == "output":
-	# output: read lyrics to stdout
-	if lyricsfile is not None:
-		sys.stdout = open(lyricsfile, "w")
-		
+	if lyricsfile is none:
+		f = sys.stdout
+	else:
+		f = open(lyricsfile, "w")
+
 	for file in files:
 		lyrics = read_id3(file)
-		if lyrics is None:
-			continue
-		for line in lyrics.splitlines():
-			print line.strip().encode("utf-8")
+		if lyrics is None: continue
+
+		sys.stdout.write(lyrics.encode("utf-8"))
+
+		# splitting required to bypass Windows text-mode stdio fuckage
+		#for line in lyrics.splitlines():
+		#	print line.strip().encode("utf-8")
 			
 elif mode == "kill":
-	# kill: remove lyrics
 	for file in files:
 		write_id3(file, None)

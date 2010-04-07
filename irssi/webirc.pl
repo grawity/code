95cb7eb98d566ca2d9a9d14b17184efca035437b
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-04-07 21:01:29 +0300

    webirc.pl now actually works

--- irssi/webirc.pl
+++ irssi/webirc.pl
@@ -3,35 +3,34 @@ use strict;
 use Irssi;
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "0.5";
+$VERSION = "0.6";
 %IRSSI = (
-	authors     => 'Mantas Mikulėnas',
-	contact     => 'grawity@gmail.com',
 	name        => 'webirc.pl',
 	description => 'Implements WEBIRC authentication for UnrealIRCd',
 	license     => 'WTFPL v2 <http://sam.zoy.org/wtfpl/>',
-	url         => 'http://purl.oclc.org/NET/grawity/irssi.html',
 );
 
-%auth = (
-	cluenet => "youwish",
+my %auth = (
+	blahnet => "youwish",
 );
 
-%fakeinfo = (
-	cluenet => [ "localhost", "127.0.0.1" ],
+my %fakeinfo = (
+	blahnet => ["fbi.gov", "127.0.0.1"],
 );
+
+sub webirc {
+	my ($server, $password, $fake) = @_;
+	my ($hostname, $ip) = @$fake;
+	$ip =~ s/^:/::/;
+	$server->print("", "Setting $hostname as hostname");
+	$server->send_raw_now("WEBIRC $password cgiirc $hostname $ip");
+}
 
 Irssi::signal_add_last "server connected" => sub {
 	my ($server) = @_;
 	my $tag = lc $server->{tag};
 	if (defined $auth{$tag} and defined $fakeinfo{$tag}) {
-		webirc $server, $auth{$tag}, "cgiirc", @{$fakeinfo{$tag}};
+		webirc $server, $auth{$tag}, "cgiirc", $fakeinfo{$tag};
 	}
-}
-
-sub webirc {
-	my ($server, $password, $hostname, $ip) = @_;
-	$server->print("", "Attempting WEBIRC authentication");
-	$server->send_raw_now("WEBIRC $password cgiirc $hostname $ip");
 }
 

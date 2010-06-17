c4fe4da0d5e858fe496feb69935b54f2979f192b
Mantas Mikulėnas <grawity@gmail.com>
2010-06-17 17:25:15 +0300

    Get rid of -a|-x options

--- tools/settermtitle.pl
+++ tools/settermtitle.pl
@@ -3,33 +3,30 @@ use strict;
 use Getopt::Long;
 
 sub msg_usage {
-	print STDERR "Usage: settermtitle [-e] [-a|-x] <title>\n";
+	print STDERR "Usage: settermtitle [-e] <title>\n";
 	return 2;
 }
 
 my $ESC = "\e";
-my $ST_ANSI = "\e\\";
-my $ST_XTERM = "\007";
-my $ST = \$ST_ANSI;
+my $ST = "\e\\";
+my $BEL = "\007";
 
 sub titlestring {
 	$_ = $ENV{TERM} // "dumb";
 	/^screen/
-		and return "${ESC}k%s${$ST}";
+		and return "${ESC}k%s${ST}";
 	(/^[xkE]term/ or /^rxvt/)
-		and return "${ESC}]0;%s${$ST}";
+		and return "${ESC}]0;%s${BEL}";
 	/^vt300/
-		and return "${ESC}]21;%s${$ST}";
+		and return "${ESC}]21;%s${ST}";
 }
 
 GetOptions(
 	'e|escape' => sub {
 		$ESC = "\\e";
-		$ST_ANSI = "\\e\\\\";
-		$ST_XTERM = "\\007";
+		$ST = "\\e\\\\";
+		$BEL = "\\007";
 		},
-	'a|ansi-st' => sub { $ST = \$ST_ANSI },
-	'x|xterm-st' => sub { $ST = \$ST_XTERM },
 );
 
 my $title = shift;

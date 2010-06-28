28ea33ee84670c199866279c5ad27d8a0c968a45
Mantas Mikulėnas <grawity@gmail.com>
2010-06-28 22:05:52 +0300

    Add nickcmp.pl

---
+++ irssi/irc-nickcmp.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# Not an Irssi script; just a code snippet.
+
+sub lci ($$) {
+	my ($s, $map) = @_;
+	if ($map eq 'rfc1459') { $s =~ tr/\[\\\]/{|}/; }
+	if ($map eq 'rfc2812') { $s =~ tr/\[\\\]^/{|}~/; }
+	return lc $s;
+}
+
+sub uci ($$) {
+	my ($s, $map) = @_;
+	if ($map eq 'rfc1459') { $s =~ tr/\{\|\}/[\\]/; }
+	if ($map eq 'rfc2812') { $s =~ tr/\{\|\}~/[\\]^/; }
+	return uc $s;
+}
+
+# Compare two nicknames on a given server
+sub nick_compare ($$$) {
+	my ($server, $a, $b) = @_;
+	my $casemap = $server->isupport("CASEMAPPING") // "rfc1459";
+	return lci $a, $casemap cmp lci $b, $casemap;
+}

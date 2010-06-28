1ee7e93cab704deb0623e507252aac9f0ee09c71
Mantas Mikulėnas <grawity@gmail.com>
2010-06-28 20:08:37 +0300

    Don't dump ps help screen if one of the PIDs is missing

--- tools/getsession.sh
+++ tools/getsession.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Print the current session's parent process name.
 
 #pid=${1:-$(ps -p $$ -o "ppid=")}
@@ -6,8 +6,11 @@ pid=${1:-$$}
 
 # our Session ID (= PID of whatever started the session)
 sid=$(ps -p $pid -o "sess=")
+[ "$sid" ] || exit 1
+
 # session starter's Parent PID (usually sshd or in.telnetd)
 sppid=$(ps -p $sid -o "ppid=")
+[ "$sppid" ] || exit 1
 
 ps -p $sppid -o "cmd="
 #ps -p $sppid -o "uid,pid,ppid,cmd"

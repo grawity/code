b04a46a92bb8c653e182fd0e2184db3f6160208a
Mantas Mikulėnas <grawity@gmail.com>
2010-08-28 12:41:56 +0300

    Finally a sane TCP and UDP listener.

--- irssi/notify/notify-receive.pl
+++ irssi/notify/notify-receive.pl
@@ -2,27 +2,41 @@
 # r20100710
 use strict;
 #use brain;
+use Getopt::Long qw(:config bundling);
 use IO::Socket;
+
+my ($dbus, $libn_serv, $libnotify);
+
+sub forked(&) {
+	my $code = shift;
+	my $pid = fork();
+	if ($pid == 0) {
+		exit &$code;
+	} else {
+		return $pid;
+	}
+}
 
 sub xml_escape($) {
 	my ($_) = @_; s/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; return $_;
 }
 
-### DBus libnotify
+sub handle_message($) {
+	my ($message) = @_;
+	my ($appname, $icon, $title, $text) = split / \| /, $message;
 
-my ($dbus, $libnotify, $dobject);
-eval {
-	require Net::DBus;
-	$dbus = Net::DBus->session;
-	$libnotify = $dbus->get_service("org.freedesktop.Notifications");
-	$dobject = $libnotify->get_object("/org/freedesktop/Notifications");
-};
+	if ($title eq "") { next; }
+	#if ($appname eq "") { $appname = "unknown"; }
+
+	send_libnotify($appname, $icon, $title, $text);
+}
 
 sub send_libnotify($$$$) {
 	my ($appname, $icon, $title, $text) = @_;
 	$text = xml_escape($text);
-	if (defined $dobject) {
-		$dobject->Notify($appname, 0, $icon, $title, $text, [], {}, 3000);
+
+	if (defined $libnotify) {
+		$libnotify->Notify($appname, 0, $icon, $title, $text, [], {}, 3000);
 	}
 	else {
 		my @args = ("notify-send");
@@ -35,44 +49,77 @@ sub send_libnotify($$$$) {
 	}
 }
 
+if (eval {require Net::DBus}) {
+	$dbus = Net::DBus->session;
+}
+
 if (defined $dbus) {
-	print "DBus: using Net::DBus\n";
-}
-else {
-	print "DBus: Net::DBus not available, falling back to notify-send\n";
+	$libn_serv = $dbus->get_service("org.freedesktop.Notifications");
+	$libnotify = $libn_serv->get_object("/org/freedesktop/Notifications");
 }
 
-### UDP listener (the main loop)
+### main loop
 
-my $ListenPort = shift @ARGV // 22754;
+my $listen = shift @ARGV;
 
-my $socket = IO::Socket::INET->new(
-	Proto => 'udp',
-	LocalPort => $ListenPort,
-	Reuse => 1,
-) or die "socket error: $!";
+sub socket_inet($$$) {
+	my ($proto, $laddr, $lport) = @_;
 
-my ($message, $title, $text);
-
-print "Waiting for notifications on *:$ListenPort\n";
-
-while ($socket->recv($message, 1024)) {
-	my ($port, $host) = sockaddr_in($socket->peername);
-	$host = join '.', unpack "C*", $host;
-
-	chomp $message;
-	my ($appname, $icon, $title, $text) = split " | ", $message, 4;
-
-	print "from:  $host:$port\n";
-	print "app:   $appname\n";
-	
-	if ($title eq "") { next; }
-	#if ($appname eq "") { $appname = "unknown"; }
-	
-	print "title: $title\n";
-	print "text:  $text\n";
-
-	send_libnotify($appname, $icon, $title, $text);
+	my $sock;
+	my %sock_args = (
+		Proto => $proto,
+		LocalAddr => $laddr,
+		LocalPort => $lport,
+		ReuseAddr => 1,
+	);
+	if (eval {require IO::Socket::INET6}) {
+		$sock = IO::Socket::INET6->new(%sock_args)
+			or die "socket: $!";
+	} elsif ($laddr =~ /:/) {
+		die "IO::Socket::INET6 required for IPv6\n";
+	} else {
+		$sock = IO::Socket::INET->new(%sock_args)
+			or die "socket: $!";
+	}
+	return $sock;
 }
-die "recv error: $!";
 
+sub accept_stream($) {
+	my $sock = shift;
+	while (my $insock = $sock->accept) {
+		forked {
+			chomp(my $data = <$insock>);
+			close $insock;
+			$data and handle_message($data);
+		};
+	}
+}
+
+sub accept_dgram($) {
+	my $sock = shift;
+	while ($sock->recv(my $data, 1024)) {
+		chomp $data;
+		$data and handle_message($data);
+	}
+}
+
+if ($listen =~ /^(tcp|udp)!(.+)!(.+)$/) {
+	my $proto = $1;
+	my $laddr = ($2 eq '*'? undef : $2);
+	my $lport = $3;
+
+	my $sock;
+
+	if ($proto eq 'tcp') {
+		$sock = socket_inet($proto, $laddr, $lport);
+		$sock->listen(1);
+		accept_stream $sock;
+	} elsif ($proto eq 'udp') {
+		$sock = socket_inet($proto, $laddr, $lport);
+		accept_dgram $sock;
+	}
+
+	close $sock;
+} else {
+	die "Unknown protocol\n";
+}

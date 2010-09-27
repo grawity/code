227d6e9496ca3a285dd7f6af49e431e4970d7123
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-09-27 23:14:13 +0300

    rwho/json: Don't output internal use fields.

--- useless/rwho/json.php
+++ useless/rwho/json.php
@@ -7,5 +7,10 @@ $host = $_GET["host"];
 
 $data = retrieve($user, $host);
 
+foreach ($data as &$row) {
+	unset($row["rowid"]);
+	unset($row["_summary"]);
+}
+
 header("Content-Type: text/plain; charset=utf-8");
 print json_encode($data)."\n";

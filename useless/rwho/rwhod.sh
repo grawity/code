e6279e2f11fb350cc7ba139e650a92fb18046ca6
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-11-16 00:00:27 +0200

    rwho: make the .sh a little more like other initscripts

--- useless/rwho/rwhod.sh
+++ useless/rwho/rwhod.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-RWHOD=(~/code/useless/rwho/rwhod.pl)
-PIDFILE=~/tmp/rwhod-$(hostname).pid
-
-if [[ -z $PERL5LIB ]]; then
-	PERL5LIB=~/lib/perl5:~/usr/lib/perl5
+if (( $(id -u) > 0 )); then
+	[[ $PIDFILE ]] ||
+		export PIDFILE="$HOME/tmp/rwhod-$(hostname).pid"
+	[[ $PERL5LIB ]] ||
+		export PERL5LIB="$HOME/lib/perl5:$HOME/usr/lib/perl5"
+else
+	[[ $PIDFILE ]] ||
+		export PIDFILE="/var/run/rwhod.pid"
 fi
-export PERL5LIB
+
+[[ $RWHOD_DIR ]] ||
+	RWHOD_DIR="$HOME/code/useless/rwho"
+[[ $RWHOD_OPTIONS ]] ||
+	RWHOD_OPTIONS=()
 
 ctl() {
 	case $1 in
 	start)
-		"${RWHOD[@]}" & pid=$!
+		"$RWHOD_DIR/rwhod.pl" "${RWHOD_OPTIONS[@]}" & pid=$!
 		echo $pid > "$PIDFILE"
 		kill -0 $pid
 		;;
@@ -23,14 +30,15 @@ ctl() {
 		;;
 	status)
 		if [[ -f $PIDFILE ]] && pid=$(< "$PIDFILE"); then
-			if kill -0 $pid; then
+			if kill -0 $pid 2>/dev/null; then
 				echo "running: pid $pid"
+				return 0
 			else
 				echo "unsure: pid $pid but can't signal"
 				return 2
 			fi
 		else
-			echo "not running or no pidfile"
+			echo "not running or no pidfile at '$PIDFILE'"
 			return 1
 		fi
 	esac

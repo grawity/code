85bf4ae03d146cc39bec4d620fd80fcba45e1f10
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-12-14 23:15:55 +0200

    fetch-user-keys: only fetch own keys when uid>999; also fix ownership security hole

--- cluenet/fetch-user-keys.php
+++ cluenet/fetch-user-keys.php
@@ -2,54 +2,76 @@
 <?php
 const LDAP_URI = "ldap://ldap.cluenet.org";
 
-const FILTER = "(&
+class config {
+	static $optin = true;
+	static $logmax = LOG_NOTICE;
+	static $logopts = LOG_PID;
+}
+
+function putlog($level, $message) {
+	if ($level <= config::$logmax)
+		syslog($level, $message);
+}
+
+$argv0 = array_shift($argv);
+foreach (getopt("av") as $opt => $optarg)
+	switch ($opt) {
+	case "a":
+		config::$optin = false;
+		break;
+	case "v":
+		config::$logmax++;
+		config::$logopts |= LOG_PERROR;
+		break;
+	}
+
+$filter = "(&
 	(objectClass=posixAccount)
 	(!(objectClass=suspendedUser))
 	(clueSshPubKey=*)
 )";
 
-class config {
-	static $optin = true;
-}
-
-openlog("fetch-keys", LOG_PERROR|LOG_PID, LOG_DAEMON);
-
-$argv0 = array_shift($argv);
-$opts = getopt("a");
-if (isset($opts["a"]))
+if (($my_uid = posix_getuid()) >= 1000) {
+	$my_pwent = posix_getpwuid($my_uid);
+	$username = $my_pwent["name"];
+	$filter = "(&{$filter}(uid=$username))";
 	config::$optin = false;
+	config::$logopts |= LOG_PERROR;
+}
 
 $ldapconf = "/etc/ldap/cluenet.conf";
 if (is_file($ldapconf))
 	putenv("LDAPCONF=$ldapconf");
 
+openlog("fetch-keys", config::$logopts, LOG_DAEMON);
+
 $conn = ldap_connect(LDAP_URI);
 if (!$conn) {
-	syslog(LOG_ERR, "LDAP connection failed");
+	putlog(LOG_ERR, "LDAP connection failed");
 	exit(1);
 }
 if (!ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3)) {
-	syslog(LOG_ERR, "upgrade to LDAPv3 failed");
+	putlog(LOG_ERR, "upgrade to LDAPv3 failed");
 	exit(1);
 }
 if (!ldap_start_tls($conn)) {
-	syslog(LOG_ERR, "TLS negotiation failed: ".ldap_error($conn));
+	putlog(LOG_ERR, "TLS negotiation failed: ".ldap_error($conn));
 	exit(1);
 }
 if (!ldap_bind($conn, null, null)) {
-	syslog(LOG_ERR, "anonymous bind failed: ".ldap_error($conn));
+	putlog(LOG_ERR, "anonymous bind failed: ".ldap_error($conn));
 	exit(1);
 }
 
-$search = ldap_list($conn, "ou=people,dc=cluenet,dc=org", FILTER,
+$search = ldap_list($conn, "ou=people,dc=cluenet,dc=org", $filter,
 	array("uid", "uidNumber", "homeDirectory", "clueSshPubKey"));
 if (!$search) {
-	syslog(LOG_ERR, "search failed: ".ldap_error($conn));
+	putlog(LOG_ERR, "search failed: ".ldap_error($conn));
 	exit(1);
 }
 
 $num_res = ldap_count_entries($conn, $search);
-syslog(LOG_INFO, "found $num_res accounts with keys");
+putlog(LOG_INFO, "found $num_res accounts");
 
 for ($entry = ldap_first_entry($conn, $search);
 		$entry != false;
@@ -64,26 +86,38 @@ for ($entry = ldap_first_entry($conn, $search);
 	if (!is_dir($home))
 		continue;
 
-	if ($optin and !file_exists("$home/.ssh/ldap_autofetch"))
-		continue;
-
-	$file = "$home/.ssh/authorized_keys";
-	$fh = fopen($file, "w");
-	if (!$fh) {
-		syslog(LOG_NOTICE, "failed to open $file");
+	if (config::$optin and !file_exists("$home/.ssh/ldap_autofetch")) {
+		putlog(LOG_INFO, "skipping $user - not opted in");
 		continue;
 	}
 
-	fwrite($fh, "# updated ".date("r")." from LDAP\n");
-	foreach ($keys as $key)
-		fwrite($fh, "$key\n");
+	putlog(LOG_INFO, "updating keys for $user");
+	$file = "$home/.ssh/authorized_keys";
+
+	$owner = fileowner($file);
+	if ($owner and $owner !== $uid) {
+		putlog(LOG_WARNING, "skipping $user - insecure ownership on $file");
+		continue;
+	}
+
+	$fh = fopen($file, "w");
+	if (!$fh) {
+		putlog(LOG_NOTICE, "failed to open $file");
+		continue;
+	}
+
+	fwrite($fh, "# updated ".date("r")." from LDAP (uid=$user)\n");
+	for ($i = 0; $i < $keys["count"]; $i++)
+		fwrite($fh, $keys[$i]."\n");
+	putlog(LOG_INFO, "wrote ".$keys["count"]." keys to $file");
 
 	$local = "$file.local";
 	if (is_file($local)) {
 		$local_fh = fopen($local, "r");
 		if (!$local_fh) {
-			syslog(LOG_NOTICE, "failed to open $local");
+			putlog(LOG_NOTICE, "failed to open $local");
 		} else {
+			putlog(LOG_INFO, "copying keys from $local");
 			fwrite($fh, "# local keys\n");
 			while (($buf = fread($local_fh, 4096)) !== false) {
 				fwrite($fh, $buf);

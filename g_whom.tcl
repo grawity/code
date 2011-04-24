f86b709f544c5bca32901ebad125615be260579b
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-04-24 16:17:37 +0300

    whom: simplify implementation

--- g_whom.tcl
+++ g_whom.tcl
@@ -2,30 +2,8 @@
 
 bind dcc o "w" dcc:whom
 proc dcc:whom {hand idx arg} {
-	set handlen 9
-	set format "%1s%-*s   %-*s  %s"
-	putdcc $idx [format $format\
-		""\
-		$handlen "Nick"\
-		$handlen "Bot"\
-		"Host"\
-	]
-	putdcc $idx [format $format\
-		"-"\
-		$handlen [string repeat "-" $handlen]\
-		$handlen [string repeat "-" $handlen]\
-		[string repeat "-" 20]\
-	]
-
-	foreach line [whom *] {
-		# hand bot host flag idle away pchan
-		putdcc $idx [format $format\
-			[lindex $line 3]\
-			$handlen [lindex $line 0]\
-			$handlen [lindex $line 1]\
-			[lindex $line 2]\
-		]
-	}
+	if {$arg == ""} {set arg "*"}
+	*dcc:whom $hand $idx $arg
 }
 
 bind dcc n "netstat" dcc:netstat

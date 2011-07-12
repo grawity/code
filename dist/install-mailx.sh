e6e739354e746ac2330a07eae0fa5b9d8b4ca67e
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-07-12 12:36:18 +0300

    bootstrap: deal nicely with existing sources/outputs

--- dist/install-mailx.sh
+++ dist/install-mailx.sh
@@ -13,7 +13,7 @@ curl 'http://nail.cvs.sourceforge.net/viewvc/nail/?view=tar' | tar xz
 makeflags=( PREFIX="$LOCAL" SYSCONFDIR="$CONFIG" )
 
 cd "nail/nail"
-patch -p1 <<'EOF'
+patch -p1 -N <<'EOF'
 --- nail.orig/openssl.c	2009-05-27 00:04:15.000000000 +0300
 +++ nail/openssl.c	2011-07-12 11:12:02.000000000 +0300
 @@ -216,9 +216,7 @@ ssl_select_method(const char *uhp)
@@ -28,6 +28,7 @@ patch -p1 <<'EOF'
  		else if (equal(cp, "tls1"))
  			method = TLSv1_client_method();
 EOF
+make clean
 make "${makeflags[@]}" IPv6="-DHAVE_IPv6_FUNCS"
 
 # install

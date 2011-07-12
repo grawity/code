52d2f72a39d85fb75566f6ffb404a069b65e5aec
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-07-12 11:48:02 +0300

    bootstrap: fix SYSCONFDIR

--- dist/install-mailx.sh
+++ dist/install-mailx.sh
@@ -10,6 +10,8 @@ curl 'http://nail.cvs.sourceforge.net/viewvc/nail/?view=tar' | tar xz
 
 # build
 
+makeflags=( PREFIX="$LOCAL" SYSCONFDIR="$CONFIG" )
+
 cd "nail/nail"
 patch -p1 <<'EOF'
 --- nail.orig/openssl.c	2009-05-27 00:04:15.000000000 +0300
@@ -26,12 +28,8 @@ patch -p1 <<'EOF'
  		else if (equal(cp, "tls1"))
  			method = TLSv1_client_method();
 EOF
-make IPv6="-DHAVE_IPv6_FUNCS"
+make "${makeflags[@]}" IPv6="-DHAVE_IPv6_FUNCS"
 
 # install
 
-make \
-	PREFIX="$LOCAL" \
-	SYSCONFDIR="$CONFIG" \
-	UCBINSTALL="install" \
-	install
+make "${makeflags[@]}" UCBINSTALL="install" install

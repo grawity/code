64386e84457a1315a861fd19098ffae8dca9404a
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-11-08 20:39:34 +0200

    spawn: add lockfile support
    Not entirely unlike flock(1), but simpler (no unnecessary shell process, -xn defaults)

--- misc/spawn.c
+++ misc/spawn.c
@@ -1,25 +1,104 @@
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/file.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+char *arg0;
 
 static void usage() {
-	fprintf(stderr, "usage: spawn [-w] <command> [args]\n");
+	fprintf(stderr, "usage: %s [-L] [-l name] [-w] <command> [args]\n", arg0);
 	exit(2);
 }
 
-int main(int argc, char *argv[]) {
-	int do_wait = 0;
-	char **cmd = &argv[1];
-
-	// very quick and dirty
-	if (!cmd[0]) usage();
-	if (!strcmp(cmd[0], "-w")) {
-		do_wait = 1;
-		cmd++;
+char * get_ttyname() {
+	char *d, *i;
+	if ((d = getenv("DISPLAY"))) {
+		*rindex(d, '.') = 0;
+		return d;
 	}
-	if (!cmd[0]) usage();
+	if ((d = ttyname(2))) {
+		i = d;
+		while (*i++)
+			if (*i == '/')
+				*i = '-';
+		return d + 5;
+	}
+	return "batch";
+}
+
+char * get_lockfile(char *name, int shared) {
+	char *dir = getenv("XDG_RUNTIME_DIR");
+	if (dir == NULL) {
+		fprintf(stderr, "%s: XDG_RUNTIME_DIR not set, cannot use lockfile\n", arg0);
+		exit(3);
+	}
+	char *disp;
+	if (shared)
+		disp = "shared";
+	else
+		disp = get_ttyname();
+	char *path;
+	if (asprintf(&path, "%s/%s.%s.lock", dir, name, disp))
+		return path;
+}
+
+int main(int argc, char *argv[]) {
+	char **cmd;
+	int do_wait = 0;
+	int do_lock = 0;
+	char *lockname;
+	int lockshared = 0;
+	char *lockfile;
+	int opt;
+	int lockfd;
+
+	arg0 = argv[0];
+
+	while ((opt = getopt(argc, argv, "+Ll:w")) != -1) {
+		switch (opt) {
+		case 'L':
+			lockshared = 1;
+			break;
+		case 'l':
+			do_lock = 1;
+			if (!strcmp(optarg, "-"))
+				lockname = NULL;
+			else
+				lockname = optarg;
+			break;
+		case 'w':
+			do_wait = 1;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	if (optind >= argc) {
+		fprintf(stderr, "%s: must specify a command to run\n", arg0);
+		usage();
+	} else {
+		cmd = &argv[optind];
+	}
+
+	if (do_lock) {
+		if (!lockname)
+			lockname = cmd[0];
+		lockfile = get_lockfile(lockname, lockshared);
+		if ((lockfd = open(lockfile, O_RDONLY|O_CREAT, 0600)) < 0) {
+			perror("open(lockfile)");
+			return 1;
+		}
+		if (flock(lockfd, LOCK_EX|LOCK_NB) < 0) {
+			if (errno != EWOULDBLOCK)
+				perror("flock");
+			return 1;
+		}
+	}
 
 	switch (fork()) {
 	case 0:

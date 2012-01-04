e8f89e2d719c024fa06b7c1cb5fddbb29a3db2e2
Mantas Mikulėnas <grawity@nullroute.eu.org>
2012-01-05 00:07:15 +0200

    more stuff

--- bin/libbleh.bash
+++ bin/libbleh.bash
@@ -1,6 +1,20 @@
 #!bash
 
+if [[ $LIBBLEH == 'y' ]]; then
+	return
+else
+	LIBBLEH=y
+fi
+
 progname=${0##*/}
+
+if (( DEBUG )); then
+	debug() {
+		echo "${progname}[$$]: $*" >&2
+	}
+else
+	debug() { :; }
+fi
 
 log() {
 	echo "* $*"
@@ -20,3 +34,21 @@ die() {
 	err "$@"
 	exit 1
 }
+
+dump_stack() {
+	echo "call stack:"
+	for i in "${!BASH_SOURCE[@]}"; do
+		echo "... ${BASH_SOURCE[i]}:${BASH_LINENO[i]} @ ${FUNCNAME[i-1]}"
+	done
+} >&2
+
+use() {
+	for lib; do
+		debug "loading lib$lib"
+		. "lib${lib}.bash"
+	done
+}
+
+if (( DEBUG )); then
+	debug "libbleh loaded by $0"
+fi

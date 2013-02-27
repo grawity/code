c9a439b300f388bfb09d9e1174253681a1f2f254
Mantas Mikulėnas <grawity@gmail.com>
2013-02-27 18:25:03 +0200

    kc: remove junk, fix file mode

--- kerberos/kc.bash
+++ kerberos/kc.bash
@@ -4,12 +4,5 @@
 # Must be 'source'd (ie. from bashrc) in order for cache switching to work.
 
 kc() {
-	local ev
-	{ ev=$(~/code/kerberos/kc.pl "$@" 3>&1 >&4); } 4>&1 \
-	&& eval "$ev"
+	local ev; { ev=$(~/code/kerberos/kc.pl "$@" 3>&1 >&4); } 4>&1 && eval "$ev"
 }
-
-if [[ -t 0 && -t 1 && $- != *i* ]]; then
-	echo "kc needs to be sourced (e.g. from your ~/.bashrc) for it to work."
-	false
-fi

c878ec6b869215ceea25ff4f0c1c809349977140
Mantas Mikulėnas <grawity@gmail.com>
2013-03-10 16:09:13 +0200

    irssi: scrollwarn: add missing import

--- irssi/scrollwarn.pl
+++ irssi/scrollwarn.pl
@@ -2,8 +2,9 @@ use strict;
 use utf8;
 use vars qw($VERSION %IRSSI);
 use Irssi;
+use Irssi::TextUI;
 
-$VERSION = '0.3';
+$VERSION = '0.4';
 %IRSSI = (
 	name		=> 'scrollwarn',
 	description	=> 'Warns you if you were scrolled up when sending a message.',
@@ -11,23 +12,16 @@ $VERSION = '0.3';
 	license		=> 'WTFPL v2 <http://sam.zoy.org/wtfpl/>',
 );
 
-sub check {
-	my ($win) = @_;
+Irssi::signal_add("send text" => sub {
+	my ($line, $server, $witem) = @_;
+
+	my $win = Irssi::active_win;
 	my $view = $win->view;
 	if (!$view->{bottom}) {
 		my $lines = $view->{ypos} - $view->{height};
 		$win->command("scrollback end");
 		$win->print("You were scrolled up by $lines lines. Message not sent.",
 			MSGLEVEL_CLIENTERROR);
-		return 0;
-	} else {
-		return 1;
-	}
-}
-
-Irssi::signal_add("send text" => sub {
-	my ($line, $server, $witem) = @_;
-	if (!check(Irssi::active_win)) {
 		Irssi::signal_stop;
 	}
 });

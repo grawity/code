54e593cb2d499c5abde8b07e01537725d437d5cc
Mantas Mikulėnas <grawity@gmail.com>
2013-05-10 10:31:15 +0300

    libfunlink: implement unlinkat()

--- system/libfunlink.c
+++ system/libfunlink.c
@@ -1,8 +1,11 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int unlink(const char *path) {
 	static int (*real_unlink)(const char*);
@@ -21,4 +24,33 @@ int unlink(const char *path) {
 		return rename(path, new);
 	else
 		return 0;
+}
+
+int unlinkat(int dirfd, const char *pathname, int flags) {
+	static int (*real_unlinkat)(int, const char*, int);
+	struct stat tmpst, pathst;
+	char *newname;
+
+	if (stat("/tmp", &tmpst) < 0)
+		goto real;
+
+	if (fstatat(dirfd, pathname, &pathst, AT_SYMLINK_NOFOLLOW) < 0)
+		goto real;
+
+	if (tmpst.st_dev != pathst.st_dev)
+		goto real;
+
+	if (asprintf(&newname, "%s~", pathname) <= 0)
+		goto real;
+
+	if (renameat(dirfd, pathname, dirfd, newname) < 0)
+		goto real;
+
+	return 0;
+
+real:
+	if (!real_unlinkat)
+		real_unlinkat = dlsym(RTLD_NEXT, "unlinkat");
+
+	return real_unlinkat(dirfd, pathname, flags);
 }

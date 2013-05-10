850ae33e17ea34e44f9ab515fc36256f20643f81
Mantas Mikulėnas <grawity@gmail.com>
2013-05-10 10:31:03 +0300

    libfunlink: compare the absolute path

--- system/libfunlink.c
+++ system/libfunlink.c
@@ -1,13 +1,19 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int unlink(const char *path) {
 	static int (*real_unlink)(const char*);
+	const char *real;
 	char *new;
 
-	if (strncmp(path, "/tmp/", 5)) {
+	real = realpath(path, NULL);
+	if (!real)
+		real = path;
+
+	if (strncmp(real, "/tmp/", 5)) {
 		if (!real_unlink)
 			real_unlink = dlsym(RTLD_NEXT, "unlink");
 		return real_unlink(path);

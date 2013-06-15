4f0b7d9514ceb86963801e0cdd71bb23a3d1b86d
Mantas Mikulėnas <grawity@gmail.com>
2013-06-15 22:24:08 +0300

    util.h: steal a piece of systemd's _cleanup_ code

--- misc/util.h
+++ misc/util.h
@@ -1,8 +1,16 @@
 #include <string.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int mkdir_p(const char *, mode_t);
 
 static inline int streq(const char *a, const char *b) {
 	return strcmp(a, b) == 0;
 }
+
+static inline void freep(void *p) {
+	free(*(void**)p);
+}
+
+#define _cleanup_(f)	__attribute__((cleanup(f)))
+#define _cleanup_free_	_cleanup_(freep)

52ad468eee2ecf452194d6d9c9b3df656d9e5460
Mantas Mikulėnas <grawity@gmail.com>
2013-09-28 00:59:13 +0300

    IRC.pm: start porting the IRC parser to Perl

---
+++ lib/perl5/Nullroute/IRC.pm
@@ -0,0 +1,43 @@
+package Nullroute::IRC;
+use warnings;
+use strict;
+
+sub split_line {
+	my ($str) = @_;
+	chomp($str);
+	my @vec = split(/ /, $str);
+	my ($i, $n) = (0, scalar @vec);
+	my ($tags, $prefix, @argv);
+	while ($i < $n && !length($vec[$i])) {
+		++$i;
+	}
+	if ($i < $n && $vec[$i] =~ /^@/) {
+		push @argv, $vec[$i];
+		++$i;
+		while ($i < $n && !length($vec[$i])) {
+			++$i;
+		}
+	}
+	if ($i < $n && $vec[$i] =~ /^:/) {
+		push @argv, $vec[$i];
+		++$i;
+		while ($i < $n && !length($vec[$i])) {
+			++$i;
+		}
+	}
+	while ($i < $n) {
+		if ($vec[$i] =~ /^:/) {
+			last;
+		} elsif (length($vec[$i])) {
+			push @argv, $vec[$i];
+		}
+		++$i;
+	}
+	if ($i < $n) {
+		my $trailing = join(" ", @vec[$i..$#vec]);
+		push @argv, substr($trailing, 1);
+	}
+	return @argv;
+}
+
+1;

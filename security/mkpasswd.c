1a1feff891134d29241f186ab9192fd3606a9729
Mantas Mikulėnas <grawity@gmail.com>
2013-11-05 09:34:00 +0200

    mkpasswd: fail if /dev/urandom cannot be read

--- security/mkpasswd.c
+++ security/mkpasswd.c
@@ -10,13 +10,34 @@ char base64[] = "abcdefghijklmnopqrstuvwxyz"
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		"0123456789./";
 
+#define RAND_DEV "/dev/urandom"
+
+int randsalt(void *buf, ssize_t len) {
+	int fd, n;
+
+	fd = open(RAND_DEV, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "mkpasswd: %s: %m\n", RAND_DEV);
+		return 0;
+	}
+
+	n = read(fd, buf, len);
+	if (n < len) {
+		fprintf(stderr, "mkpasswd: %s: short read (%m)\n", RAND_DEV);
+		close(fd);
+		return 0;
+	}
+
+	close(fd);
+	return 1;
+}
+
 char *makesalt(char algo) {
 	char buf[12], salt[20];
-	unsigned i=0, j=0, fd;
+	unsigned i=0, j=0;
 
-	fd = open("/dev/urandom", O_RDONLY);
-	read(fd, buf, 12);
-	close(fd);
+	if (!randsalt(buf, sizeof(buf)))
+		return NULL;
 
 	salt[j++] = '$';
 	salt[j++] = algo;
@@ -55,6 +76,9 @@ int main(int argc, char *argv[]) {
 
 	if (!salt)
 		salt = makesalt('5');
+	if (!salt)
+		return 1;
+
 	if (!passwd)
 		passwd = getpass("Password: ");
 

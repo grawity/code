6744fa2f056bdf4b773f8fec06888b53c8672ed3
Mantas Mikulėnas <grawity@gmail.com>
2013-11-26 18:12:32 +0200

    lib.py: Python 2 compatibility hacks

--- lib/python/nullroute/__init__.py
+++ lib/python/nullroute/__init__.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import sys
 
 def warn(*args):
-    print("\033[1;33mwarning:\033[m", *args, file=sys.stderr)
+    print("\033[1;33mwarning:\033[m", " ".join(args), file=sys.stderr)
 
 def err(*args):
-    print("\033[1;31merror:\033[m", *args, file=sys.stderr)
+    print("\033[1;31merror:\033[m", " ".join(args), file=sys.stderr)
 
 def die(*args):
     err(*args)

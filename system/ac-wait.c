028bd39d1301a28a9320efac3e74a5805c42a93c
Mantas Mikulėnas <grawity@gmail.com>
2013-11-30 19:23:44 +0200

    ac-wait: revert cleanup as initial check was needed
    This reverts commit 3ab8190abe635c95ca5d785b9d4b5c13fc37afb2.

--- system/ac-wait.c
+++ system/ac-wait.c
@@ -4,13 +4,28 @@
 #include <libudev.h>
 #include <poll.h>
 
+#define IS_ONE(x) ((x) && *(x) == '1')
+
 int main(void) {
 	struct udev *udev;
 	struct udev_monitor *mon;
+	struct udev_device *dev;
 	struct pollfd pf[1];
+	const char *value;
 	int ret;
 
 	udev = udev_new();
+
+	dev = udev_device_new_from_subsystem_sysname(udev, "power_supply", "AC0");
+
+	value = udev_device_get_sysattr_value(dev, "online");
+
+	if (IS_ONE(value)) {
+		printf("online, waiting\n");
+	} else {
+		printf("offline, exiting\n");
+		return 0;
+	}
 
 	mon = udev_monitor_new_from_netlink(udev, "udev");
 
@@ -19,29 +34,27 @@ int main(void) {
 			.events = POLLIN,
 		};
 
-	udev_monitor_filter_add_match_subsystem_devtype(mon, "power_supply", NULL);
+	udev_monitor_filter_add_match_subsystem_devtype(mon,
+							udev_device_get_subsystem(dev),
+							udev_device_get_devtype(dev));
 	
 	udev_monitor_enable_receiving(mon);
 
 	while ((ret = poll(pf, 1, -1)) >= 0) {
 		struct udev_device *dev;
-		const char *sysname, *value;
-		bool is_online;
+		const char *sysname;
 
 		dev = udev_monitor_receive_device(mon);
-
 		sysname = udev_device_get_sysname(dev);
 		if (strncmp(sysname, "AC", 2) != 0)
 			continue;
-
 		value = udev_device_get_sysattr_value(dev, "online");
-		is_online = (value && *value == '1');
 
-		if (is_online) {
+		if (IS_ONE(value)) {
 			printf("online\n");
 		} else {
 			printf("offline\n");
-			break;
+			return 0;
 		}
 	}
 

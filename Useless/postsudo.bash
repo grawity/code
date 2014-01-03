e7ef100e2d577aa149bbdaf5cbcd5103fd2f1ae6
Mantas Mikulėnas <grawity@gmail.com>
2014-01-03 18:30:16 +0200

    snapshot

--- Useless/postsudo.bash
+++ Useless/postsudo.bash
@@ -1,3 +1,5 @@
 # source this in bash
 
-_p() case $BASH_COMMAND in *' NOW') eval sudo ${BASH_COMMAND% *} && false;; *) true; esac; shopt -s extdebug; trap _p DEBUG
+#_p() case $BASH_COMMAND in *' NOW') eval sudo ${BASH_COMMAND% *} && false; esac; shopt -s extdebug; trap _p DEBUG
+
+shopt -s extdebug; trap 'case $BASH_COMMAND in *" NOW") eval sudo ${BASH_COMMAND% *} && false; esac' DEBUG

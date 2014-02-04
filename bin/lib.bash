f389f9a07aec537ecde8f08a5a1a1a735b9a8d0d
Mantas Mikulėnas <grawity@gmail.com>
2014-02-04 08:30:02 +0200

    lib.bash: set defaults for XDG variables
    Do not export to environment, just set for the script itself.

--- bin/lib.bash
+++ bin/lib.bash
@@ -12,6 +12,13 @@ fi
 
 _lvl=$(( LVL++ )); export LVL
 
+## Variable defaults
+
+: ${XDG_CACHE_HOME:=$HOME/.cache}
+: ${XDG_CONFIG_HOME:=$HOME/.config}
+: ${XDG_DATA_HOME:=$HOME/.local/share}
+: ${XDG_DATA_DIRS:=/usr/local/share:/usr/share}
+
 ## Logging
 
 progname=${0##*/}

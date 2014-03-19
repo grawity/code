f81ff17f8b6741d5499a6c1433814d7608a07fcb
Mantas Mikulėnas <grawity@gmail.com>
2014-03-20 00:32:10 +0200

    KitchenSink.pm: add sd_notify()

--- lib/perl5/Nullroute/KitchenSink.pm
+++ lib/perl5/Nullroute/KitchenSink.pm
@@ -1,6 +1,8 @@
 package Nullroute::KitchenSink;
 use common::sense;
 use base "Exporter";
+use IO::Socket::UNIX;
+
 use constant {
 	DATE_FMT_MBOX	=> '%a %b %_d %H:%M:%S %Y',
 	DATE_FMT_MIME	=> '%a, %d %b %Y %H:%M:%S %z',
@@ -10,6 +12,20 @@ use constant {
 our @EXPORT = qw(
 	DATE_FMT_MBOX
 	DATE_FMT_MIME
-	);
+	sd_notify
+);
+
+sub sd_notify {
+	if (my $path = $ENV{NOTIFY_SOCKET}) {
+		$path =~ s/^@/\0/;
+		my $data = join("\n", @_);
+		my $sock = IO::Socket::UNIX->new(Peer => $path,
+		                                 Type => SOCK_DGRAM);
+		if ($sock) {
+			$sock->send($data);
+			$sock->close;
+		}
+	}
+}
 
 1;

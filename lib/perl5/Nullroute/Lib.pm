47317a3a04d36cf305b8d5417b8e49f73c76c9a2
Mantas Mikulėnas <grawity@gmail.com>
2014-05-04 17:16:01 +0300

    Lib.pm: whitespace

--- lib/perl5/Nullroute/Lib.pm
+++ lib/perl5/Nullroute/Lib.pm
@@ -48,19 +48,19 @@ sub _msg {
 	warn "${nameprefix}${color}${prefix}:${reset} ${msg}\n";
 }
 
-sub _debug { _msg("debug", shift, "\e[36m") if $::debug; }
+sub _debug  { _msg("debug", shift, "\e[36m") if $::debug; }
 
-sub _info  { _msg("info", shift, "\e[1;34m") if $::debug; }
+sub _info   { _msg("info", shift, "\e[1;34m") if $::debug; }
 
 sub _notice { _msg("notice", shift, "\e[1;35m"); }
 
-sub _warn  { _msg("warning", shift, "\e[1;33m"); ++$::warnings; }
+sub _warn   { _msg("warning", shift, "\e[1;33m"); ++$::warnings; }
 
-sub _err   { _msg("error", shift, "\e[1;31m"); ++$::errors; }
+sub _err    { _msg("error", shift, "\e[1;31m"); ++$::errors; }
 
-sub _die   { _err(shift); exit 1; }
+sub _die    { _err(shift); exit 1; }
 
-sub _usage { _msg("usage", ($::arg0 . " " . shift), ""); }
+sub _usage  { _msg("usage", ($::arg0 . " " . shift), ""); }
 
 sub forked (&) { fork || exit shift->(); }
 

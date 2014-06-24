5e94e6dde1b6d5f8f976bfae5a1a0bb9870a1c71
Mantas Mikulėnas <grawity@gmail.com>
2014-06-24 20:27:23 +0300

    Lib.pm: make the argument order of _msg() more sane

--- lib/perl5/Nullroute/Lib.pm
+++ lib/perl5/Nullroute/Lib.pm
@@ -34,7 +34,7 @@ $::errors = 0;
 my $seen_usage = 0;
 
 sub _msg {
-	my ($prefix, $msg, $color) = @_;
+	my ($msg, $prefix, $color) = @_;
 
 	my $color = (-t 2) ? $color : "";
 	my $reset = (-t 2) ? "\e[m" : "";
@@ -59,7 +59,7 @@ sub _msg {
 sub _fmsg {
 	return _msg(@_) if $::debug;
 
-	my ($prefix, $msg, $color, $fmt_prefix, $fmt_color) = @_;
+	my ($msg, $prefix, $color, $fmt_prefix, $fmt_color) = @_;
 
 	my $color = (-t 2) ? $fmt_color : "";
 	my $reset = (-t 2) ? "\e[m" : "";
@@ -68,21 +68,21 @@ sub _fmsg {
 	warn "${nameprefix}${color}${fmt_prefix}${reset} ${msg}\n";
 }
 
-sub _debug  { _msg("debug", shift, "\e[36m") if $::debug; }
+sub _debug  { _msg(shift, "debug", "\e[36m") if $::debug; }
 
-sub _info   { _msg("info", shift, "\e[1;34m") if $::debug; }
+sub _info   { _msg(shift, "info", "\e[1;34m") if $::debug; }
 
-sub _log    { _fmsg("log", shift, "\e[32m", "--", "\e[32m"); }
+sub _log    { _fmsg(shift, "log", "\e[32m", "--", "\e[32m"); }
 
-sub _notice { _msg("notice", shift, "\e[1;35m"); }
+sub _notice { _msg(shift, "notice", "\e[1;35m"); }
 
-sub _warn   { _msg("warning", shift, "\e[1;33m"); ++$::warnings; }
+sub _warn   { _msg(shift, "warning", "\e[1;33m"); ++$::warnings; }
 
-sub _err    { _msg("error", shift, "\e[1;31m"); ++$::errors; }
+sub _err    { _msg(shift, "error", "\e[1;31m"); ++$::errors; }
 
 sub _die    { _err(shift); exit 1; }
 
-sub _usage  { _msg("usage", ($::arg0 . " " . shift), ""); }
+sub _usage  { _msg($::arg0." ".shift, "usage", ""); }
 
 sub forked (&) { fork || exit shift->(); }
 

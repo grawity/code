497184bdbfae3883ab541360cd18f8f5f720eba9
Mantas Mikulėnas <grawity@gmail.com>
2014-07-09 08:47:57 +0300

    put common functions in Nullroute::LDAP
    I have some stuff in Cluenet::LDAP and UK::LDAP that ought to be shared,
    not copypasted.

---
+++ lib/perl5/Nullroute/LDAP.pm
@@ -0,0 +1,30 @@
+package Nullroute::LDAP;
+use base "Exporter";
+use Nullroute::Lib;
+
+@EXPORT = qw(
+	ldap_check
+);
+
+sub ldap_format_error {
+	my ($res, $dn) = @_;
+
+	utf8::decode($res->error);
+	my $text = "LDAP error: ".$res->error."\n";
+	$text .= "\tcode: ".$res->error_name."\n" if $::debug;
+	$text .= "\tfailed: $dn\n"                if $dn;
+	$text .= "\tmatched: ".$res->dn."\n"      if $res->dn;
+	return $text;
+}
+
+sub ldap_check {
+	my ($res, $dn) = @_;
+
+	if ($res->is_error) {
+		log_done();
+		my $text = ldap_format_error($res, $dn);
+		_die($text);
+	}
+}
+
+1;

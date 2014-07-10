8870e8434ccc84541956d76de6a2d4c403cb02f2
Mantas Mikulėnas <grawity@gmail.com>
2014-07-10 19:26:44 +0300

    lib.py, mksshfp: count errors, set exit code apropriately

--- lib/python/nullroute/__init__.py
+++ lib/python/nullroute/__init__.py
@@ -1,19 +1,27 @@
 from __future__ import print_function
 import sys
 
+num_warnings = 0
+num_errors = 0
+
 def _log(prefix, msg, color=""):
     print("\033[%sm%s:\033[m %s" % (color, prefix, msg),
           file=sys.stderr)
 
 def debug(msg):
-    _log("debug", msg, "36")
+    return _log("debug", msg, "36")
 
 def warn(msg):
-    _log("warning", msg, "1;33")
+    num_warnings += 1
+    return _log("warning", msg, "1;33")
 
 def err(msg):
-    _log("error", msg, "1;31")
+    num_errors += 1
+    return _log("error", msg, "1;31")
 
 def die(msg):
     _log("fatal", msg, "1;31")
     sys.exit(1)
+
+def exit():
+    sys.exit(1 if num_errors else 0)

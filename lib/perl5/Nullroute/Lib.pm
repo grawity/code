aeb98acd5e3fa267575579bf88b996ed2d7eca66
Mantas Mikulėnas <grawity@gmail.com>
2014-09-03 19:18:11 +0300

    Lib.pm: stop printing the (wrong) file:line

--- lib/perl5/Nullroute/Lib.pm
+++ lib/perl5/Nullroute/Lib.pm
@@ -54,15 +54,12 @@ sub _msg {
 
 	if ($prefix eq "debug" || $::debug >= 2) {
 		my $skip = ($opt{skip} || 0) + 1;
-		my @frame;
 		my $func;
 		do {
-			@frame = caller(++$skip);
+			my @frame = caller(++$skip);
 			$func = $frame[3] // "main";
 		} while ($func =~ /::__ANON__$/);
 		$func =~ s/^main:://;
-		$prefix .= " @ ".($frame[1] // "?").":".($frame[2] // "?")
-			if $::debug >= 3;
 		$prefix .= " ($func)";
 	}
 	elsif ($prefix eq "usage" && !$::debug && $seen_usage++) {

8e82ef8140a11534874d97e0f18742a9c4361e45
Mantas Mikulėnas <grawity@gmail.com>
2014-12-27 16:11:16 +0200

    sec.py: return complete information as dict

--- lib/python/nullroute/sec.py
+++ lib/python/nullroute/sec.py
@@ -1,8 +1,10 @@
 import subprocess
 
 def get_netrc(machine, login=None):
-    cmd = ["getnetrc", "-d", "-n", "-f", "%l\n%p", machine]
+    cmd = ["getnetrc", "-d", "-n", "-f", "%m\n%l\n%p\n%a", machine]
     if login is not None:
         cmd.append(login)
     r = subprocess.check_output(cmd)
-    return r.decode("utf-8").split("\n")
+    keys = ["machine", "login", "password", "account"]
+    vals = r.decode("utf-8").split("\n")
+    return dict(zip(keys, vals))

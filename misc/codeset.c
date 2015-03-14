7f18667dca9efe870381c1b42b95cb568f6c6c18
Mantas Mikulėnas <grawity@gmail.com>
2015-03-14 23:44:49 +0200

    codeset: add option to print date format

--- misc/codeset.c
+++ misc/codeset.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <locale.h>
 #include <langinfo.h>
 
 int main(int argc, char *argv[]) {
+	int opt;
+	nl_item do_langinfo = CODESET;
+
+	while ((opt = getopt(argc, argv, "D")) != -1) {
+		switch (opt) {
+		case 'D':
+			do_langinfo = _DATE_FMT;
+			break;
+		case '?':
+			return 2;
+		}
+	}
+
 	while (*++argv)
 		putenv(*argv);
 
 	setlocale(LC_ALL, "");
 
-	printf("%s\n", nl_langinfo(CODESET));
+	printf("%s\n", nl_langinfo(do_langinfo));
 
 	return 0;
 }

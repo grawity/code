61681efb905c150662c4e582f7854fb65e9b41ba
Mantas Mikulėnas <grawity@gmail.com>
2015-03-17 09:02:29 +0200

    lib.py: move logging stuff to ui.py

--- lib/python/nullroute/__init__.py
+++ lib/python/nullroute/__init__.py
@@ -1,34 +1,11 @@
 from __future__ import print_function
 import sys
+from nullroute.ui import (debug, warn, err, die)
 
 arg0 = sys.argv[0].split("/")[-1]
 
-num_warnings = 0
-num_errors = 0
-
 def window_title(msg):
     print("\033]2;%s\007" % msg, file=sys.stderr)
-
-def _log(prefix, msg, color=""):
-    print("\033[%sm%s:\033[m %s" % (color, prefix, msg),
-          file=sys.stderr)
-
-def debug(msg):
-    return _log("debug", msg, "36")
-
-def warn(msg):
-    global num_warnings
-    num_warnings += 1
-    return _log("warning", msg, "1;33")
-
-def err(msg):
-    global num_errors
-    num_errors += 1
-    return _log("error", msg, "1;31")
-
-def die(msg):
-    _log("fatal", msg, "1;31")
-    sys.exit(1)
 
 def exit():
     global num_errors
--- lib/python/nullroute/ui.py
+++ lib/python/nullroute/ui.py
@@ -9,6 +9,33 @@ except ImportError:
 _stderr_tty = None
 _stderr_width = None
 
+_num_warnings = 0
+_num_errors = 0
+
+## regular log messages
+
+def _log(prefix, msg, color=""):
+    print("\033[%sm%s:\033[m %s" % (color, prefix, msg), file=sys.stderr)
+
+def debug(msg):
+    return _log("debug", msg, "36")
+
+def warn(msg):
+    global _num_warnings
+    _num_warnings += 1
+    return _log("warning", msg, "1;33")
+
+def err(msg):
+    global _num_errors
+    _num_errors += 1
+    return _log("error", msg, "1;31")
+
+def die(msg):
+    _log("fatal", msg, "1;31")
+    sys.exit(1)
+
+## status/progress messages
+
 def isatty():
     global _stderr_tty
     if _stderr_tty is None:

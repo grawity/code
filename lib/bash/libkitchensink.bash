e13e46d6042e66fb3a1cfb98ef5940fd7da73db6
Mantas Mikulėnas <grawity@gmail.com>
2015-07-17 13:21:34 +0300

    move sshrun to libkitchensink

--- lib/bash/libkitchensink.bash
+++ lib/bash/libkitchensink.bash
@@ -7,3 +7,13 @@ ks:setattr() {
 	local file=$1 name=$2 value=$3
 	setfattr "$file" --name="user.$name" --value="$value"
 }
+
+ks:sshrun() {
+	local host=$1 argv=("${@:2}")
+	local -i i
+	for (( i = 0; i < ${#argv[@]}; i++ )); do
+		printf -v argv[i] '%q' "${argv[i]}"
+	done
+	debug "running \"${argv[*]}\" on $host"
+	ssh "$host" "${argv[*]}"
+}

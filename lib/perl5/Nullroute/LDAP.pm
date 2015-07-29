f82108152922fb939ebe17d4b2be3227a8e9870b
Mantas Mikulėnas <grawity@gmail.com>
2015-07-29 14:00:07 +0300

    LDAP.pm: add convenience functions, including atomic increment

--- lib/perl5/Nullroute/LDAP.pm
+++ lib/perl5/Nullroute/LDAP.pm
@@ -5,8 +5,60 @@ use base "Exporter";
 use Nullroute::Lib;
 
 @EXPORT = qw(
+    ldap_read_attr
+    ldap_cas_attr
+    ldap_increment_attr
     ldap_check
 );
+
+sub ldap_read_attr {
+    my ($conn, $dn, $attr) = @_;
+    my $res;
+
+    $res = $conn->search(base => $dn,
+                         scope => "base",
+                         filter => "(objectClass=*)",
+                         attrs => [$attr]);
+    ldap_check($res);
+
+    if ($res->count > 0) {
+        return $res->entry(0)->get_value($attr);
+    } else {
+        return undef;
+    }
+}
+
+sub ldap_cas_attr {
+    my ($conn, $dn, $attr, $old, $new) = @_;
+    my $res;
+
+    $res = $conn->modify($dn,
+        delete => { $attr => $old },
+        add => { $attr => $new },
+    );
+    ldap_check($res, $dn,
+        ["LDAP_NO_SUCH_ATTRIBUTE",
+         "LDAP_TYPE_OR_VALUE_EXISTS"]);
+
+    return !$res->is_error;
+}
+
+sub ldap_increment_attr {
+    my ($conn, $dn, $attr, $incr) = @_;
+    my $done;
+    my $val;
+
+    $incr ||= 1;
+    $done = false;
+    until ($done) {
+        _debug("fetching $attr");
+        $val = ldap_read_attr($conn, $dn, $attr);
+        _debug("fetched '$val', swapping");
+        $done = ldap_cas_attr($conn, $dn, $attr, $val, $val+$incr);
+        _debug($done ? "finished" : "retrying");
+    }
+    return $val+$incr;
+}
 
 sub ldap_format_error {
     my ($res, $dn) = @_;

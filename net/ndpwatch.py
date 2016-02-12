7e8d18ca0a2923b358f91038e7705fa4a8882602
Mantas Mikulėnas <grawity@gmail.com>
2016-02-12 14:31:07 +0200

    add WIP ndpwatch.py script

---
+++ net/ndpwatch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from ipaddress import *
+from pprint import pprint
+from nullroute.sys.ifconfig import *
+
+## main
+
+hosts = [
+    ("uk-pf-aukstaiciu9", SshConnector, FreeBsdNeighbourTable),
+    ("uk-pf-ausros73", SshConnector, FreeBsdNeighbourTable),
+    ("uk-pf-m18-adm", SshConnector, FreeBsdNeighbourTable),
+    ("uk-pf-m18-stud", SshConnector, FreeBsdNeighbourTable),
+    ("uk-pf-maironio7", SshConnector, FreeBsdNeighbourTable),
+    ("uk-pf-utenio2", SshConnector, FreeBsdNeighbourTable),
+    #("uk-untangle", SshConnector, LinuxNeighbourTable),
+    #("uk-nas1", SshConnector, SolarisNeighbourTable),
+]
+
+ip_mac = {}
+mac_ip = {}
+
+for host, conn_type, nt_type in hosts:
+    nt = nt_type(conn_type("root@%s" % host))
+    for item in nt.get_ndp6():
+        ip = item["ip"]
+        mac = item["mac"]
+        if item["ip"].startswith("fe80:"):
+            continue
+        ip_mac[item["ip"]] = item["mac"]
+
+pprint(ip_mac)

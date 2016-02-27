2bb607bd7deb9828defb3ec42c09c60f7e1d74a0
Mantas Mikulėnas <grawity@gmail.com>
2016-02-27 21:21:43 +0200

    rebuild-parttable: fix parted syntax

--- hacks/System/rebuild-parttable.sh
+++ hacks/System/rebuild-parttable.sh
@@ -18,5 +18,5 @@ for part in /sys/class/block/${dev}[0-9]*; do
 	end=$((start+size-1))
 	echo -e "\n${c[0]}# partition ${c[1]}$num${c[0]}, start ${c[2]}$start${c[0]}, size ${c[3]}$size${c[0]}, end ${c[4]}$end${c[0]}${c[5]}\n"
 	echo "sgdisk /dev/$dev --new=$num:$start:$end"
-	#echo "parted /dev/$dev mkpart primary $start $end"
+	#echo "parted /dev/$dev mkpart primary ${start}s ${end}s"
 done

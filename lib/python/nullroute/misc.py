0ed20949bbe8f17f8a9f4ebc3907dd70f89f0944
Mantas Mikulėnas <grawity@gmail.com>
2016-07-17 22:02:06 +0300

    misc.py: import fmt_size()

--- lib/python/nullroute/misc.py
+++ lib/python/nullroute/misc.py
@@ -1,3 +1,5 @@
+import math
+
 def filter_filename(name):
     xlat = [
         (' ', '_'),
@@ -23,3 +25,29 @@ def uniq(items):
         if item not in seen:
             seen.add(item)
             yield item
+
+def fmt_size_foo(n, d=1, u=1024):
+    cs = "BkMGTPEZYH"
+    e = 0
+    while n >= u:
+        n /= u
+        e += 1
+    c = cs[e]
+    return "%.*f%s" % (d, n, c)
+
+def fmt_size(nbytes, si=False):
+    if nbytes == 0:
+        return "zero bytes"
+    prefixes = ".kMGTPEZYH"
+    div = 1000 if si else 1024
+    exp = int(math.log(nbytes, div))
+    if exp == 0:
+        return "%.1f bytes" % nbytes
+    elif exp < len(prefixes):
+        quot = nbytes / div**exp
+        return "%.1f %sB" % (quot, prefixes[exp])
+    else:
+        exp = len(prefixes) - 1
+        quot = nbytes / div**exp
+        return "%f %sB" % (quot, prefixes[exp])
+    return str(nbytes)

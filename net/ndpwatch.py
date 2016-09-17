3103f3f8d0e668e32995cc8514c784debef59300
Mantas Mikulėnas <mantas@utenos-kolegija.lt>
2016-09-17 21:13:16 +0300

    ndpwatch: make protocol configurable

--- net/ndpwatch.py
+++ net/ndpwatch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from ipaddress import *
+from nullroute.core import *
 from nullroute.system.ifconfig import *
 from pprint import pprint
 import sqlalchemy as δ
@@ -26,6 +27,7 @@ _systems = {
 db_url = None
 hosts = []
 max_age_days = 6*30
+mode = "all"
 
 with open("/home/grawity/lib/arplog.conf") as f:
     for line in f:
@@ -39,6 +41,14 @@ with open("/home/grawity/lib/arplog.conf") as f:
             hosts.append((host_v, _connectors[conn_v], _systems[sys_v]))
         elif k == "age":
             max_age_days = int(v)
+        elif k == "mode":
+            if v in {"ipv4", "ipv6", "all", "both"}:
+                mode = v
+            else:
+                Core.die("config parameter %r has unrecognized value %r" % (k, v))
+
+if not db_url:
+    Core.die("database URL not configured")
 
 δEngine = δ.create_engine(db_url)
 δConn = δEngine.connect()
@@ -49,11 +59,18 @@ st = δ.sql.text("""
         ON DUPLICATE KEY UPDATE last_seen=:now
      """)
 
+if mode == "ipv4":
+    func = lambda nt: nt.get_arp4()
+elif mode == "ipv6":
+    func = lambda nt: nt.get_ndp6()
+else:
+    func = lambda nt: nt.get_all()
+
 for host, conn_type, nt_type in hosts:
     print("connecting to", host)
     nt = nt_type(conn_type(host))
     now = time.time()
-    for item in nt.get_all():
+    for item in func(nt):
         ip = item["ip"].split("%")[0]
         mac = item["mac"]
         if ip.startswith("fe80:"):

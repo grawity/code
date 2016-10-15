ed59facc7cd8f75cb20f2079645e4d792ec26267
Mantas Mikulėnas <grawity@gmail.com>
2016-10-15 22:30:14 +0300

    sec.py: add libsecret support (still via subprocess)

--- lib/python/nullroute/sec.py
+++ lib/python/nullroute/sec.py
@@ -1,5 +1,41 @@
 import subprocess
 
+def save_libsecret(label, secret, **attributes):
+    cmd = ["secret-tool", "store", "--label=%s" % label]
+    for k, v in attributes.items():
+        cmd += [str(k), str(v)]
+
+    if hasattr(secret, "encode"):
+        secret = secret.encode("utf-8")
+
+    with subprocess.Popen(cmd, stdin=subprocess.PIPE) as proc:
+        out, err = proc.communicate(secret)
+        ret = proc.wait()
+        if ret != 0:
+            raise IOError("libsecret store failed: (%r, %r)" % (ret, err))
+
+def get_libsecret(**attributes):
+    cmd = ["secret-tool", "lookup"]
+    for k, v in attributes.items():
+        cmd += [str(k), str(v)]
+
+    try:
+        r = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError:
+        raise KeyError("libsecret lookup failed")
+    else:
+        return r
+
+def clear_libsecret(**attributes):
+    cmd = ["secret-tool", "clear"]
+    for k, v in attributes.items():
+        cmd += [str(k), str(v)]
+
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError:
+        raise KeyError("libsecret clear failed")
+
 def get_netrc(machine, login=None, service=None):
     if service:
         machine = "%s/%s" % (service, machine)

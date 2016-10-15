bb9ddaac7f3e4e1ce2bdb796a7c865d9c110a6d5
Mantas Mikulėnas <grawity@gmail.com>
2016-10-15 22:35:56 +0300

    sec.py: use subprocess.run

--- lib/python/nullroute/sec.py
+++ lib/python/nullroute/sec.py
@@ -8,51 +8,51 @@ def save_libsecret(label, secret, attributes):
     if hasattr(secret, "encode"):
         secret = secret.encode("utf-8")
 
-    with subprocess.Popen(cmd, stdin=subprocess.PIPE) as proc:
-        out, err = proc.communicate(secret)
-        ret = proc.wait()
-        if ret != 0:
-            raise IOError("libsecret store failed: (%r, %r)" % (ret, err))
+    r = subprocess.run(cmd, input=secret,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    if r.returncode != 0:
+        raise IOError("libsecret store failed: (%r, %r)" % (r.returncode,
+                                                            r.stderr.decode()))
 
 def get_libsecret(attributes):
     cmd = ["secret-tool", "lookup"]
     for k, v in attributes.items():
         cmd += [str(k), str(v)]
 
-    try:
-        r = subprocess.check_output(cmd)
-    except subprocess.CalledProcessError:
-        raise KeyError("libsecret lookup failed")
-    else:
-        return r
+    r = subprocess.run(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    if r.returncode != 0:
+        raise KeyError("libsecret lookup failed: %r" % r.stderr.decode())
+
+    return r.stdout
 
 def clear_libsecret(attributes):
     cmd = ["secret-tool", "clear"]
     for k, v in attributes.items():
         cmd += [str(k), str(v)]
 
-    try:
-        subprocess.check_output(cmd)
-    except subprocess.CalledProcessError:
-        raise KeyError("libsecret clear failed")
+    r = subprocess.run(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    if r.returncode != 0:
+        raise KeyError("libsecret clear failed: %r" % r.stderr.decode())
 
 def get_netrc(machine, login=None, service=None):
     if service:
         machine = "%s/%s" % (service, machine)
+
     cmd = ["getnetrc", "-d", "-n", "-f", "%m\n%l\n%p\n%a", machine]
     if login:
         cmd.append(login)
 
-    try:
-        r = subprocess.check_output(cmd)
-    except subprocess.CalledProcessError:
+    r = subprocess.run(cmd, stdout=subprocess.PIPE)
+    if r.returncode != 0:
         raise KeyError("~/.netrc lookup for %r failed" % machine)
 
     keys = ["machine", "login", "password", "account"]
-    vals = r.decode("utf-8").split("\n")
-
+    vals = r.stdout.decode("utf-8").split("\n")
     if len(keys) != len(vals):
-        raise IOError("'getnetrc' returned incorrect data %r" % r)
+        raise IOError("'getnetrc' returned weird data %r" % r)
 
     return dict(zip(keys, vals))
 

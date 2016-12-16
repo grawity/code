68341c6b432039fc9a06b338b785b6e3e99d7d62
Mantas Mikulėnas <grawity@gmail.com>
2016-12-16 09:03:23 +0200

    sec/pine: while -> math

--- lib/python/nullroute/sec/pine.py
+++ lib/python/nullroute/sec/pine.py
@@ -4,23 +4,20 @@ TABSZ = (LASTCH - FIRSTCH + 1)
 
 def _xlate_in(key, char):
     if char >= FIRSTCH and char <= LASTCH:
-        key += (char - FIRSTCH)
-        while key >= TABSZ:
-            key -= TABSZ
-        return key, key + FIRSTCH
+        key = (key + char - FIRSTCH) % TABSZ
+        char = key + FIRSTCH
+        return key, char
     else:
         return key, char
 
 def _xlate_out(key, char):
     if char >= FIRSTCH and char <= LASTCH:
         char -= key
-        if char < FIRSTCH-TABSZ:
-            char += 2*TABSZ
+        if char < FIRSTCH - TABSZ:
+            char += 2 * TABSZ
         elif char < FIRSTCH:
             char += TABSZ
-        key += (char - FIRSTCH)
-        while key >= TABSZ:
-            key -= TABSZ
+        key = (key + char - FIRSTCH) % TABSZ
         return key, char
     else:
         return key, char
@@ -30,6 +27,14 @@ def decrypt_line(key, line):
     for char in line:
         char = ord(char)
         key, char = _xlate_out(key, char)
+        out += chr(char)
+    return out
+
+def encrypt_line(key, line):
+    out = ""
+    for char in line:
+        char = ord(char)
+        key, char = _xlate_in(key, char)
         out += chr(char)
     return out
 

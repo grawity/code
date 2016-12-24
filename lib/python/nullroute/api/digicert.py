71a0fe9e2c7bbab0d0046d0fd6a3246a54e86fa2
Mantas Mikulėnas <grawity@gmail.com>
2016-12-24 22:48:54 +0200

    digicert.py: add post_order
    Now we can get certificates!
    
        orgs = dc.get_organizations()
        order = {
            "certificate": {
                "common_name": "example.com",
                "csr": csr,
                "signature_hash": "sha256",
            },
            "organization": {
                "id": orgs[0]["id"],
            },
            "validity_years": 3,
        }
        status = dc.post_order("ssl_multi_domain", order)
        if status["requests"][0]["status"] == "approved":
            cert = dc.get_order_certificate(status["id"])
            print(cert)

--- lib/python/nullroute/api/digicert.py
+++ lib/python/nullroute/api/digicert.py
@@ -3,6 +3,9 @@ from pprint import pprint
 import requests
 from requests.exceptions import HTTPError
 
+class DevError(BaseException):
+    pass
+
 class CertCentralClient(object):
     # https://www.digicert.com/services/v2/documentation/authorization/authorization-list
     base = "https://www.digicert.com/services/v2"
@@ -24,6 +27,12 @@ class CertCentralClient(object):
         resp.raise_for_status()
         return resp
 
+    def post(self, ep, *args, **kwargs):
+        uri = self.base + ep
+        Core.debug("posting to %r" % uri)
+        resp = self.ua.post(uri, *args, **kwargs)
+        resp.raise_for_status()
+        return resp
 
     def get_myself(self):
         if not self._user:
@@ -37,6 +46,13 @@ class CertCentralClient(object):
     def get_container_authorizations(self, container_id):
         return self.get("/authorization",
                         params={"container_id": container_id})
+
+    def get_organizations(self):
+        resp = self.get("/organization")
+        data = resp.json()
+        if data["page"]["total"] > 1:
+            raise DevError("paging not yet implemented for %r" % data)
+        return data["organizations"]
 
     def get_order(self, order_id):
         resp = self.get("/order/certificate/%s" % order_id)
@@ -52,3 +68,7 @@ class CertCentralClient(object):
             pass
         resp = self.get("/certificate/%s/download/format/%s" % (cert_id, format))
         return cert_name, resp.content
+
+    def post_order(self, order_type, order_data):
+        resp = self.post("/order/certificate/%s" % order_type, json=order_data)
+        return resp.json()

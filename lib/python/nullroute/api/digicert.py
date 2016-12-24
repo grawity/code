367a21ec2c88be4a0f5282010078afd48005c43b
Mantas Mikulėnas <grawity@gmail.com>
2016-12-24 22:48:28 +0200

    digicert.py: remove unnecessary api indirection

--- lib/python/nullroute/api/digicert.py
+++ lib/python/nullroute/api/digicert.py
@@ -25,41 +25,30 @@ class CertCentralClient(object):
         return resp
 
 
-    def _api_get_myself(self):
-        return self.get("/user/me")
-
-    def _api_get_ctr_authorizations(self, container_id):
-        return self.get("/authorization",
-                        params={"container_id": container_id})
-
-    def _api_get_order(self, order_id):
-        return self.get("/order/certificate/%s" % order_id)
-
-    def _api_get_certificate(self, cert_id, format="p7b"):
-        return self.get("/certificate/%s/download/format/%s" % (cert_id, format))
-
     def get_myself(self):
         if not self._user:
-            self._user = self._api_get_myself().json()
+            resp = self.get("/user/me")
+            self._user = resp.json()
         return self._user
 
     def get_default_container(self):
         return self.get_myself()["container"]["id"]
 
-    def get_order(self, order_id):
-        return self._api_get_order(order_id).json()
+    def get_container_authorizations(self, container_id):
+        return self.get("/authorization",
+                        params={"container_id": container_id})
 
-    def get_order_certificate(self, order_id):
+    def get_order(self, order_id):
+        resp = self.get("/order/certificate/%s" % order_id)
+        return resp.json()
+
+    def get_order_certificate(self, order_id, format="p7b"):
         order = self.get_order(order_id)
         cert_id = order["certificate"]["id"]
-
         if order["product"]["type"] == "ssl_certificate":
             cert_name = order["certificate"]["common_name"]
             cert_names = order["certificate"]["dns_names"]
         elif order["product"]["type"] == "client_certificate":
             pass
-
-        cert_data = self._api_get_certificate(cert_id, format="p7b")
-        raw_pkcs7 = cert_data.content
-
-        return cert_name, raw_pkcs7
+        resp = self.get("/certificate/%s/download/format/%s" % (cert_id, format))
+        return cert_name, resp.content

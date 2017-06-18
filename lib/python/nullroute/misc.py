6ea3fe90750d7ab83a376b330d832471a3ef6251
Mantas Mikulėnas <grawity@gmail.com>
2017-06-18 13:59:42 +0300

    misc.py: add flatten_dict()

--- lib/python/nullroute/misc.py
+++ lib/python/nullroute/misc.py
@@ -70,6 +70,11 @@ def chunk(vec, size):
     for i in range(0, len(vec), size):
         yield vec[i:i+size]
 
+def flatten_dict(d):
+    for k, v in d.items():
+        yield k
+        yield v
+
 def uniq(items):
     seen = set()
     for item in items:

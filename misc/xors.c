1771caade6f03296cbce21c933c8d9ab57f08c2d
Mantas Mikulėnas <grawity@gmail.com>
2017-11-12 17:35:28 +0200

    Revert "xors: s/step/shift/"
    Doesn't work in my use case.
    
    This reverts commit f6d6b09af7cc5c7aad86b71fa5eac7408fce9ccf.

--- misc/xors.c
+++ misc/xors.c
@@ -20,20 +20,21 @@ void swrite(int fd, const void *buf, size_t nb) {
 int main(int argc, char *argv[]) {
 	unsigned char buf[BUFSIZE];
 	size_t buflen;
-	unsigned i, key = 0, incr = 1, shift = 0;
+	unsigned i, key = 0, incr = 1, step = 1;
 
 	if (argc < 3)
-		errx(2, "usage: xors <key> [<incr> [<shift>]]");
+		errx(2, "usage: xors <key> [<incr> [<step>]]");
 
 	if (argc > 1) key = atoi(argv[1]);
 	if (argc > 2) incr = atoi(argv[2]);
-	if (argc > 3) shift = atoi(argv[3]);
+	if (argc > 3) step = atoi(argv[3]);
 
 	while ((buflen = read(0, buf, BUFSIZE))) {
 		for (i = 0; i < buflen; i++) {
+			if (i > 0 && i % step == 0)
+				key += incr;
 			key %= 256;
-			buf[i] ^= (key >> shift);
-			key += incr;
+			buf[i] ^= key;
 		}
 		swrite(1, buf, buflen);
 	}

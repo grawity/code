3694195b9cc141a49ab6657d6ffeee2ec2bafca4
Mantas Mikulėnas <grawity@gmail.com>
2018-05-31 19:21:10 +0300

    misc.py: import simplified print_dependency_tree() from ldmissing

--- lib/python/nullroute/misc.py
+++ lib/python/nullroute/misc.py
@@ -16,3 +16,37 @@ def uniq(items):
         if item not in seen:
             seen.add(item)
             yield item
+
+def print_dependency_tree(tree, root, *, indent=0, ctx=None):
+    """
+    >>> tree = {"a": {"b", "c"},
+    >>>         "b": {"d"},
+    >>>         "c": {"b", "d"}}
+    >>> print_dependency_tree(tree, "a")
+    a
+    ├─b
+    │ └─d
+    └─c
+      ├─b
+      │ └─d
+      └─d
+    """
+    depth, branches, seen = ctx or (0, [], set())
+    if depth == 0:
+        print(" "*indent + root)
+    if root not in tree:
+        return
+    branches += [None]
+    seen |= {root}
+    children = set(tree[root]) - seen
+    more = len(children)
+    for child in sorted(children):
+        more -= 1
+        branches[depth] = ("├" if more else "└") + "─"
+        if child in seen:
+            continue
+        print(" "*indent + "".join(branches) + child)
+        if child in tree:
+            branches[depth] = ("│" if more else " ") + " "
+            ctx = depth + 1, branches.copy(), seen.copy()
+            print_dependency_tree(tree, child, indent=indent, ctx=ctx)

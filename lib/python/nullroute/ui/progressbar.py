af7f5f3ec505a2fba65a7a7ba916fd26cfb0d844
Mantas Mikulėnas <grawity@gmail.com>
2018-12-11 19:10:07 +0200

    ui/progressbar.py: throttle output to once every 0.1s

--- lib/python/nullroute/ui/progressbar.py
+++ lib/python/nullroute/ui/progressbar.py
@@ -1,12 +1,15 @@
 from math import ceil, floor
 from nullroute.string import fmt_size_short
+import time
 
 class ProgressBar():
     def __init__(self, max_bytes):
+        self.throttle = 0.1
         self.bar_width = 40
         self.cur_bytes = 0
         self.max_bytes = max_bytes
         self._max_fmt = fmt_size_short(max_bytes)
+        self._last_out = 0
 
     def print(self):
         cur_percent = 100 * self.cur_bytes / self.max_bytes
@@ -14,8 +17,12 @@ class ProgressBar():
         cur_fmt = fmt_size_short(self.cur_bytes)
         bar = "#" * ceil(cur_width) + " " * floor(self.bar_width - cur_width)
         bar = "%3.0f%% [%s] %s of %s" % (cur_percent, bar, cur_fmt, self._max_fmt)
-        print(bar, end="\r", flush=True)
+        print(bar, end="\033[K\r", flush=True)
 
     def incr(self, delta):
         self.cur_bytes += delta
-        self.print()
+
+        now = time.time()
+        if now - self._last_out >= self.throttle:
+            self.print()
+            self._last_out = now

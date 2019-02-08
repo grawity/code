9fa5f20a570ac5f9f3027f3cae9dce171d5824c0
Mantas Mikulėnas <grawity@gmail.com>
2019-02-08 17:37:17 +0200

    ldap.py: adjust constant names

--- lib/python/nullroute/ldap.py
+++ lib/python/nullroute/ldap.py
@@ -3,17 +3,16 @@ import ldap3.protocol.rfc4527
 from ldap3.utils.ciDict import CaseInsensitiveDict
 import ssl
 
-LDAP_CONTROL_POSTREAD = "1.3.6.1.1.13.2"
-
-LDAP_FEATURE_MODIFY_INCREMENT = "1.3.6.1.1.14"
+OID_LDAP_CONTROL_POSTREAD = "1.3.6.1.1.13.2"
+OID_LDAP_FEATURE_MODIFY_INCREMENT = "1.3.6.1.1.14"
 
 def connect_gssapi(host):
     serv = ldap3.Server(host,
                         tls=ldap3.Tls(validate=ssl.CERT_REQUIRED),
-                        get_info="DSA")
+                        get_info=ldap3.DSA)
     conn = ldap3.Connection(serv,
-                            authentication="SASL",
-                            sasl_mechanism="GSSAPI")
+                            authentication=ldap3.SASL,
+                            sasl_mechanism=ldap3.GSSAPI)
     conn.open()
     if not conn.start_tls():
         raise Exception("start_tls failed", conn.result)
@@ -29,7 +28,7 @@ def _has_feature(conn, oid):
 
 def read_attr(conn, dn, attr, raw=False):
     if not conn.search(dn, "(objectClass=*)",
-                       search_scope="BASE",
+                       search_scope=ldap3.BASE,
                        attributes=[attr]):
         raise Exception("search failed", conn.result)
     attr = conn.entries[0][attr]
@@ -37,8 +36,8 @@ def read_attr(conn, dn, attr, raw=False):
 
 def increment_attr(conn, dn, attr, incr=1):
     # optimization: RFC 4525 Modify-Increment + RFC 4527 Post-Read
-    if _has_control(conn, LDAP_CONTROL_POSTREAD) and \
-       _has_feature(conn, LDAP_FEATURE_MODIFY_INCREMENT):
+    if _has_control(conn, OID_LDAP_CONTROL_POSTREAD) and \
+       _has_feature(conn, OID_LDAP_FEATURE_MODIFY_INCREMENT):
         # this is far uglier than the Perl version already
         postread_ctrl = ldap3.protocol.rfc4527.post_read_control([attr])
         res = conn.modify(dn,
@@ -46,7 +45,7 @@ def increment_attr(conn, dn, attr, incr=1):
                           controls=[postread_ctrl])
         if not res:
             raise Exception("modify-increment failed", conn.result)
-        res = conn.result["controls"][LDAP_CONTROL_POSTREAD]["value"]["result"]
+        res = conn.result["controls"][OID_LDAP_CONTROL_POSTREAD]["value"]["result"]
         res = CaseInsensitiveDict(res)
         return res[attr][0]
 

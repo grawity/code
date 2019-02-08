555787925e95f6c840545d6fd59e993dfe6c74b8
Mantas Mikulėnas <grawity@gmail.com>
2019-02-08 18:07:50 +0200

    ldap: add libldap version of the ldap3 util functions

--- lib/python/nullroute/ldap/ldap3_utils.py
+++ lib/python/nullroute/ldap/ldap3_utils.py
@@ -1,21 +1,24 @@
 import ldap3
 import ldap3.protocol.rfc4527
 from ldap3.utils.ciDict import CaseInsensitiveDict
+from nullroute.core import Core
 import ssl
 
 OID_LDAP_CONTROL_POSTREAD = "1.3.6.1.1.13.2"
 OID_LDAP_FEATURE_MODIFY_INCREMENT = "1.3.6.1.1.14"
 
-def connect_gssapi(host):
-    serv = ldap3.Server(host,
+def connect_gssapi(url):
+    serv = ldap3.Server(url,
                         tls=ldap3.Tls(validate=ssl.CERT_REQUIRED),
                         get_info=ldap3.DSA)
     conn = ldap3.Connection(serv,
                             authentication=ldap3.SASL,
-                            sasl_mechanism=ldap3.GSSAPI)
+                            sasl_mechanism=ldap3.GSSAPI,
+                            raise_exceptions=True)
     conn.open()
-    if not conn.start_tls():
-        raise Exception("start_tls failed", conn.result)
+    if not url.startswith("ldaps://"):
+        if not conn.start_tls():
+            raise Exception("start_tls failed", conn.result)
     if not conn.bind():
         raise Exception("bind failed", conn.result)
     return conn
@@ -34,9 +37,10 @@ def read_attr(conn, dn, attr, raw=False):
     attr = conn.entries[0][attr]
     return attr.raw_values if raw else attr.values
 
-def increment_attr(conn, dn, attr, incr=1):
+def increment_attr(conn, dn, attr, incr=1, use_increment=True):
     # optimization: RFC 4525 Modify-Increment + RFC 4527 Post-Read
-    if _has_control(conn, OID_LDAP_CONTROL_POSTREAD) and \
+    if use_increment and \
+       _has_control(conn, OID_LDAP_CONTROL_POSTREAD) and \
        _has_feature(conn, OID_LDAP_FEATURE_MODIFY_INCREMENT):
         # this is far uglier than the Perl version already
         postread_ctrl = ldap3.protocol.rfc4527.post_read_control([attr])
@@ -62,4 +66,4 @@ def increment_attr(conn, dn, attr, incr=1):
             Core.debug("modify failed: %r", conn.result)
             wait += 1
             time.usleep(0.05 * 2**int(rand(wait)))
-    return val+incr
+    return val + incr
---
+++ lib/python/nullroute/ldap/libldap_utils.py
@@ -0,0 +1,63 @@
+import ldap
+import ldap.controls.readentry
+from nullroute.core import Core
+
+OID_LDAP_CONTROL_POSTREAD = b"1.3.6.1.1.13.2"
+OID_LDAP_FEATURE_MODIFY_INCREMENT = b"1.3.6.1.1.14"
+
+class CaseInsensitiveDict(dict):
+    def __init__(self, *args, **kwargs):
+        tmp = dict(*args, **kwargs)
+        tmp = {k.lower(): v for k, v in tmp.items()}
+        super().__init__(tmp)
+
+    def __getitem__(self, key):
+        return super().__getitem__(key.lower())
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key.lower(), value)
+
+def connect_gssapi(url):
+    conn = ldap.initialize(url)
+    if not url.startswith("ldaps://"):
+        conn.start_tls_s()
+    conn.sasl_interactive_bind_s("", ldap.sasl.gssapi())
+    return conn
+
+def read_attr(conn, dn, attr, raw=False):
+    attrs = conn.read_s(dn, attrlist=[attr])
+    values = CaseInsensitiveDict(attrs)[attr]
+    if not raw:
+        values = [v.decode() for v in values]
+    return values
+
+def increment_attr(conn, dn, attr, incr=1, use_increment=True):
+    if use_increment:
+        # TODO: this needs to be cached
+        rootDSE = conn.read_rootdse_s()
+        if OID_LDAP_CONTROL_POSTREAD in rootDSE["supportedControl"] and \
+           OID_LDAP_FEATURE_MODIFY_INCREMENT in rootDSE["supportedFeatures"]:
+            incr = str(incr).encode()
+            ctrl = ldap.controls.readentry.PostReadControl(attrList=[attr])
+            res = conn.modify_ext_s(dn,
+                                    [(ldap.MOD_INCREMENT, attr, incr)],
+                                    serverctrls=[ctrl])
+            for outctrl in res[3]:
+                if outctrl.controlType == ctrl.controlType:
+                    values = CaseInsensitiveDict(outctrl.entry)[attr]
+                    return int(values[0])
+    
+    done = False
+    wait = 0
+    while not done:
+        old_val = read_attr(conn, dn, attr, raw=True)[0]
+        new_val = str(int(old_val) + incr).encode()
+        # _cas_attr
+        done = conn.modify_s(dn,
+                             [(ldap.MOD_DELETE, attr, old_val),
+                              (ldap.MOD_ADD, attr, new_val)])
+        if not done:
+            Core.debug("modify failed: %r", conn.result)
+            wait += 1
+            time.usleep(0.05 * 2**int(rand(wait)))
+    return int(new_val)

09b973229251418c8ea04609ae03ed5cd46fd0e3
Mantas Mikulėnas <grawity@gmail.com>
2019-03-19 16:14:18 +0200

    ProgressBar: allow output file handle to be overridden

--- lib/python/nullroute/ui/progressbar.py
+++ lib/python/nullroute/ui/progressbar.py
@@ -3,13 +3,14 @@ from nullroute.string import fmt_size_short
 import time
 
 class ProgressBar():
-    def __init__(self, max_value, *, fmt_func=None):
+    def __init__(self, max_value, *, file=None, fmt_func=None):
         self.bar_width = 40
         self.cur_value = 0
         self.max_value = max_value
         self._fmt_func = fmt_func or fmt_size_short
         self._max_fmt = self._fmt_func(max_value)
 
+        self.output_fh = file or sys.stdout
         self.delay = 0
         self.throttle = 0.1
         self._first_in = 0
@@ -21,7 +22,7 @@ class ProgressBar():
         cur_fmt = self._fmt_func(self.cur_value)
         bar = "#" * ceil(cur_width) + " " * floor(self.bar_width - cur_width)
         bar = "%3.0f%% [%s] %s of %s" % (cur_percent, bar, cur_fmt, self._max_fmt)
-        print(bar, end="\033[K\r", flush=True)
+        print(bar, end="\033[K\r", file=self.output_fh, flush=True)
 
     def incr(self, delta):
         self.cur_value += delta
@@ -37,4 +38,4 @@ class ProgressBar():
             self._last_out = now
 
     def end(self, hide=False):
-        print("\033[K" if hide else "", end="", flush=True)
+        print("\033[K" if hide else "", end="", file=self.output_fh, flush=True)

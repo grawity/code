b6e87a940e6aed06adb9bbc6822ebfaa79d0a7ef
Mantas Mikulėnas <grawity@gmail.com>
2019-03-22 17:08:01 +0200

    api/python_web: add fanbox-related stuff

---
+++ lib/python/nullroute/api/pixiv_web.py
@@ -0,0 +1,86 @@
+from nullroute.core import Core, Env
+from nullroute.api.base import PersistentAuthBase
+from nullroute.scrape import Scraper
+import nullroute.sec
+import os
+import requests
+import time
+
+def parse_query_string(query):
+    return {k: requests.utils.unquote(v)
+            for (k, v) in [x.split("=", 1)
+                           for x in query.split("&")]}
+
+class PixivWebClient(Scraper, PersistentAuthBase):
+    TOKEN_SCHEMA = "org.eu.nullroute.BearerToken"
+    TOKEN_NAME = "Pixiv web cookie"
+    TOKEN_DOMAIN = "www.pixiv.net"
+    TOKEN_PATH = Env.find_cache_file("pixiv_web.auth.json")
+
+    def __init__(self):
+        super().__init__()
+        self.user_id = None
+        self._authenticate()
+
+    def _load_creds(self):
+        creds = nullroute.sec.get_netrc_service("pixiv.net", "http")
+        return creds
+
+    def _authenticate(self):
+        if self.user_id:
+            Core.warn("BUG: _authenticate() called twice")
+            return True
+
+        token = self._load_token()
+        if token:
+            if os.environ.get("FORCE_TOKEN_REFRESH"):
+                token_valid = False
+            else:
+                token_valid = token["expires"] > time.time()
+
+            if token_valid:
+                cookie = requests.cookies.create_cookie(**token)
+                Core.debug("loaded cookie: %r", cookie)
+                self.ua.cookies.set_cookie(cookie)
+                Core.debug("verifying session status")
+                resp = self.get("https://www.pixiv.net/member.php", allow_redirects=False)
+                if resp.is_redirect:
+                    url = requests.utils.urlparse(resp.next.url)
+                    if url.path == "/member.php":
+                        query = parse_query_string(url.query)
+                        self.user_id = int(query["id"])
+                        Core.debug("session is valid, userid %r", self.user_id)
+                        return True
+            else:
+                Core.debug("cookie has expired")
+
+        creds = self._load_creds()
+        if creds:
+            Core.info("logging in to Pixiv as %r", creds["login"])
+            page = self.get_page("https://accounts.pixiv.net/login?lang=en")
+            key = page.select_one("input[name='post_key']")["value"]
+
+            page = self.ua.post("https://accounts.pixiv.net/api/login?lang=en",
+                                data={"post_key": key,
+                                      "pixiv_id": creds["login"],
+                                      "password": creds["password"]})
+            print(page)
+
+            cookie = self.ua.cookies._cookies[".pixiv.net"]["/"]["PHPSESSID"]
+            token = {a: getattr(cookie, a)
+                     for a in ["version", "name", "value", "port", "domain", "path",
+                               "secure", "expires", "rfc2109"]}
+            Core.debug("token = %r", token)
+            self._store_token(token)
+
+    def get_fanbox_creator(self, user_id):
+        resp = self.get("https://www.pixiv.net/ajax/fanbox/creator",
+                        params={"userId": post_id})
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_fanbox_post(self, post_id):
+        resp = self.get("https://www.pixiv.net/ajax/fanbox/post",
+                        params={"postId": post_id})
+        resp.raise_for_status()
+        return resp.json()

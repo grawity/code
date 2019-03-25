bcdcef422ca7ee4eca089a94cf56f815f157e902
Mantas Mikulėnas <grawity@gmail.com>
2019-03-25 09:16:34 +0200

    progressbar.py: implement an indefinite progress bar

--- lib/python/nullroute/scrape.py
+++ lib/python/nullroute/scrape.py
@@ -39,17 +39,16 @@ def file_ext(url):
 def _progress_bar(iterable, max_bytes, chunk_size):
     #hide_complete = (max_bytes < 1*1024*1024)
     hide_complete = True
-    if max_bytes is not None:
+    if max_bytes is None:
+        from nullroute.ui.progressbar import IndefiniteProgressBar
+        bar = IndefiniteProgressBar()
+    else:
         from nullroute.ui.progressbar import ProgressBar
         bar = ProgressBar(max_value=max_bytes)
-        for i in iterable:
-            yield i
-            bar.incr(len(i))
-        bar.end(hide_complete)
-    else:
-        # TODO: indefinite progress bar
-        for i in iterable:
-            yield i
+    for i in iterable:
+        yield i
+        bar.incr(len(i))
+    bar.end(hide_complete)
 
 class Scraper(object):
     def __init__(self, output_dir="."):
@@ -118,13 +117,12 @@ class Scraper(object):
             resp = self.get(url, headers=hdr, stream=True)
             with open(name + ".part", "wb") as fh:
                 num_bytes = resp.headers.get("content-length")
-                chunk_size = 1024
                 if num_bytes is not None:
                     num_bytes = int(num_bytes)
                 else:
                     Core.debug("response has no size, disabling progress bar")
                     Core.debug("headers: %r", resp.headers)
-                    chunk_size = 32*1024
+                chunk_size = 1024
                 for chunk in _progress_bar(resp.iter_content(chunk_size),
                                            max_bytes=num_bytes,
                                            chunk_size=chunk_size):
--- lib/python/nullroute/ui/progressbar.py
+++ lib/python/nullroute/ui/progressbar.py
@@ -7,9 +7,9 @@ class ProgressBar():
     def __init__(self, max_value, *, file=None, fmt_func=None):
         self.bar_width = 40
         self.cur_value = 0
-        self.max_value = max_value
+        self.max_value = max_value or 0
         self._fmt_func = fmt_func or fmt_size_short
-        self._max_fmt = self._fmt_func(max_value)
+        self._max_fmt = self._fmt_func(self.max_value)
 
         self.output_fh = file or sys.stderr
         self.delay = 0
@@ -49,13 +49,50 @@ class ProgressBar():
             bar.incr(1)
         bar.end(True)
 
+class IndefiniteProgressBar(ProgressBar):
+    def __init__(self, *args, **kwargs):
+        super().__init__(0, *args, **kwargs)
+
+    def print(self):
+        ship = "-=-"
+        tmp = self.bar_width - len(ship)
+        cur_width = tmp - abs(self.cur_value % (tmp * 2) - tmp)
+        cur_fmt = self._fmt_func(self.cur_value)
+        bar = " " * ceil(cur_width) + ship
+        bar = " ??%% [%*s] %s" % (-self.bar_width, bar, cur_fmt)
+        print(bar, end="\033[K\r", file=self.output_fh, flush=True)
+
+    def incr(self, delta):
+        self.cur_value += 1
+
+        now = time.time()
+        if not self._first_in:
+            self._first_in = now
+        if now - self._first_in >= self.delay and \
+           now - self._last_out >= self.throttle:
+            self.print()
+            self._last_out = now
+
 class ProgressText(ProgressBar):
     def __init__(self, *args, fmt="%s/%s", **kwargs):
         super().__init__(*args, **kwargs)
+        self.fmt = fmt
         self.throttle = 0
 
     def print(self):
-        bar = self.fmt % (self.cur_value, self.max_value)
+        cur_fmt = self._fmt_func(self.cur_value)
+        bar = self.fmt % (cur_fmt, self._max_fmt)
+        print(bar, end="\033[K\r", file=self.output_fh, flush=True)
+
+class IndefiniteProgressText(ProgressBar):
+    def __init__(self, *args, fmt="%s", **kwargs):
+        super().__init__(*args, max_value=0, **kwargs)
+        self.fmt = fmt
+        self.throttle = 0
+
+    def print(self):
+        cur_fmt = self._fmt_func(self.cur_value)
+        bar = self.fmt % (cur_fmt,)
         print(bar, end="\033[K\r", file=self.output_fh, flush=True)
 
 def progress_iter(iterable, *args, **kwargs):

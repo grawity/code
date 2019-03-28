1f83952301328219cb70db0f463451b6e81951c1
Mantas Mikulėnas <grawity@gmail.com>
2019-03-28 13:37:58 +0200

    PersistentAuthBase: allow TOKEN_SCHEMA to be empty, not that it really should be

--- lib/python/nullroute/api/base.py
+++ lib/python/nullroute/api/base.py
@@ -9,33 +9,36 @@ class PersistentAuthBase():
     TOKEN_PATH = None
 
     def _load_token(self):
-        try:
-            data = nullroute.sec.get_libsecret({"xdg:schema": self.TOKEN_SCHEMA,
-                                                "domain": self.TOKEN_DOMAIN})
-            Core.debug("found %s in keyring", self.TOKEN_NAME)
-            return json.loads(data)
-        except KeyError:
-            if self.TOKEN_PATH:
-                try:
-                    with open(self.TOKEN_PATH, "r") as fh:
-                        data = json.load(fh)
-                    Core.debug("found %s in filesystem", self.TOKEN_NAME)
-                    return data
-                except FileNotFoundError:
-                    pass
-                except Exception as e:
-                    Core.debug("could not load %r: %r", self.TOKEN_PATH, e)
-                    self._forget_token()
+        if self.TOKEN_SCHEMA:
+            try:
+                data = nullroute.sec.get_libsecret({"xdg:schema": self.TOKEN_SCHEMA,
+                                                    "domain": self.TOKEN_DOMAIN})
+                Core.debug("found %s in keyring", self.TOKEN_NAME)
+                return json.loads(data)
+            except KeyError:
+                pass
+        if self.TOKEN_PATH:
+            try:
+                with open(self.TOKEN_PATH, "r") as fh:
+                    data = json.load(fh)
+                Core.debug("found %s in filesystem", self.TOKEN_NAME)
+                return data
+            except FileNotFoundError:
+                pass
+            except Exception as e:
+                Core.debug("could not load %r: %r", self.TOKEN_PATH, e)
+                self._forget_token()
         return None
 
     def _store_token(self, data, extra=None):
         extra = (extra or {})
-        Core.debug("storing %s in keyring", self.TOKEN_NAME)
-        nullroute.sec.store_libsecret(self.TOKEN_NAME,
-                                      json.dumps(data),
-                                      {"xdg:schema": self.TOKEN_SCHEMA,
-                                       "domain": self.TOKEN_DOMAIN,
-                                       **extra})
+        if self.TOKEN_SCHEMA:
+            Core.debug("storing %s in keyring", self.TOKEN_NAME)
+            nullroute.sec.store_libsecret(self.TOKEN_NAME,
+                                          json.dumps(data),
+                                          {"xdg:schema": self.TOKEN_SCHEMA,
+                                           "domain": self.TOKEN_DOMAIN,
+                                           **extra})
         if self.TOKEN_PATH:
             Core.debug("storing %s in filesystem", self.TOKEN_NAME)
             try:
@@ -48,7 +51,8 @@ class PersistentAuthBase():
 
     def _forget_token(self):
         Core.debug("flushing auth tokens")
-        nullroute.sec.clear_libsecret({"xdg:schema": self.TOKEN_SCHEMA,
-                                       "domain": self.TOKEN_DOMAIN})
+        if self.TOKEN_SCHEMA:
+            nullroute.sec.clear_libsecret({"xdg:schema": self.TOKEN_SCHEMA,
+                                           "domain": self.TOKEN_DOMAIN})
         if self.TOKEN_PATH:
             os.unlink(self.TOKEN_PATH)

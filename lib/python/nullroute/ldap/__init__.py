282767a2a40d6e35947b18fb367dedd5e7401903
Mantas Mikulėnas <grawity@gmail.com>
2019-07-28 13:54:33 +0300

    ldap.py: misc

--- lib/python/nullroute/ldap/__init__.py
+++ lib/python/nullroute/ldap/__init__.py
@@ -4,7 +4,12 @@ except ImportError:
     from .client_ldap3 import LdapClient
 
 class NullrouteLdapClient(LdapClient):
-    root = "dc=nullroute,dc=eu,dc=org"
+    base = "dc=nullroute,dc=eu,dc=org"
 
     def __init__(self):
         super().__init__("ldaps://ldap.nullroute.eu.org")
+
+def connect_auth():
+    conn = NullrouteLdapClient()
+    conn.bind_gssapi()
+    return conn

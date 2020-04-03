71f5bcb03d44c4f336c65a884aedd96840af5a3c
Mantas Mikulėnas <grawity@gmail.com>
2020-04-03 19:16:38 +0300

    libflstat: override glibc __lxstat[64]()

--- system/libflstat.c
+++ system/libflstat.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h> /* AT_* */
 
+#if 0
 int lstat(const char *pathname, struct stat *statbuf) {
 	static int (*real_lstat)(const char *, struct stat *);
 	int result = -1;
@@ -20,6 +21,41 @@ int lstat(const char *pathname, struct stat *statbuf) {
 
 	return result;
 }
+#endif
+
+/* Actual glibc lstat() implementations */
+
+int __lxstat(int ver, const char *pathname, struct stat *statbuf) {
+	static int (*__real_lxstat)(int, const char *, struct stat *);
+	int result = -1;
+
+	if (!__real_lxstat)
+		__real_lxstat = dlsym(RTLD_NEXT, "__lxstat");
+
+	result = __xstat(ver, pathname, statbuf);
+
+	if (result < 0)
+		result = __real_lxstat(ver, pathname, statbuf);
+
+	return result;
+}
+
+int __lxstat64(int ver, const char *pathname, struct stat64 *stat64buf) {
+	static int (*__real_lxstat64)(int, const char *, struct stat64 *);
+	int result = -1;
+
+	if (!__real_lxstat64)
+		__real_lxstat64 = dlsym(RTLD_NEXT, "__lxstat64");
+
+	result = __xstat64(ver, pathname, stat64buf);
+
+	if (result < 0)
+		result = __real_lxstat64(ver, pathname, stat64buf);
+
+	return result;
+}
+
+/* Not sure if needed -- I suspect it's probably implemented in terms of statx() anyway */
 
 int fstatat(int dirfd, const char *pathname, struct stat *statbuf, int flags) {
 	static int (*real_fstatat)(int, const char *, struct stat *, int);
@@ -36,6 +72,8 @@ int fstatat(int dirfd, const char *pathname, struct stat *statbuf, int flags) {
 
 	return result;
 }
+
+/* Used by coreutils */
 
 int statx(int dirfd, const char *pathname, int flags, unsigned int mask, struct statx *statxbuf) {
 	static int (*real_statx)(int, const char *, int, unsigned int, struct statx*);

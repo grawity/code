8671badb2eedee2938dbdc5fe84727b7b7c67f97
Mantas Mikulėnas <grawity@gmail.com>
2020-09-26 21:11:33 +0300

    sasl.py: make sure the server offers SEC_NONE security layer

--- lib/python/nullroute/sec/sasl.py
+++ lib/python/nullroute/sec/sasl.py
@@ -134,6 +134,8 @@ class SaslGSSAPI(SaslMechanism):
             Core.trace("SASL-GSSAPI server token: %r (encrypted=%r, QoP=%r)", server_token, encrypted, qop)
             if len(server_token) != 4:
                 raise MechanismFailure("incorrect length for SASL-GSSAPI server token")
+            if not (server_token[0] & self.SEC_NONE):
+                raise MechanismFailure("server doesn't support not having a SASL security layer")
             # bitmask security_layers [1 byte], uint max_msg_size [3 bytes]
             # We only set bit '1' (no security layers).
             client_token = b'\x01' + b'\xFF\xFF\xFF' + (self.authz_id or "").encode("utf-8")

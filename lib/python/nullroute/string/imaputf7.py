b3bd48cd7b71d5294f19454f321a63982385840a
Mantas Mikulėnas <grawity@gmail.com>
2020-09-27 11:50:54 +0300

    imaputf7.py: import the encode function as well

--- lib/python/nullroute/string/imaputf7.py
+++ lib/python/nullroute/string/imaputf7.py
@@ -2,6 +2,37 @@
 # (c) 2014, Menno Smits
 # Released under the "New" BSD license <https://spdx.org/licenses/BSD-3-Clause.html>
 
+import binascii
+
+def encode_imap_utf7(s: str) -> bytes:
+    def base64_utf7_encode(buffer):
+        s = "".join(buffer).encode("utf-16be")
+        return binascii.b2a_base64(s).rstrip(b"\n=").replace(b"/", b",")
+
+    res = bytearray()
+    b64_buffer = []
+    for c in s:
+        # printable ascii case should not be modified
+        o = ord(c)
+        if 0x20 <= o <= 0x7e:
+            if b64_buffer:
+                res.extend(b'&' + base64_utf7_encode(b64_buffer) + b'-')
+                del b64_buffer[:]
+            # Special case: & is used as shift character so we need to escape it in ASCII
+            if o == 0x26:  # & = 0x26
+                res.extend(b'&-')
+            else:
+                res.append(o)
+        # Bufferize characters that will be encoded in base64 and append them later
+        # in the result, when iterating over ASCII character or the end of string
+        else:
+            b64_buffer.append(c)
+    # Consume the remaining buffer if the string finish with non-ASCII characters
+    if b64_buffer:
+        res.extend(b'&' + base64_utf7_encode(b64_buffer) + b'-')
+        del b64_buffer[:]
+    return bytes(res)
+
 def decode_imap_utf7(s: bytes) -> str:
     def base64_utf7_decode(s):
         s_utf7 = b"+" + s.replace(b",", b"/") + b"-"

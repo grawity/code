7399bf6d1fd65f8ebf00b6efadd0a97b37924fdf
Mantas Mikulėnas <grawity@gmail.com>
2021-09-30 08:23:11 +0300

    pixiv: import pixivpy 3.6.0

--- lib/python/pixivpy3/__init__.py
+++ lib/python/pixivpy3/__init__.py
@@ -1,11 +1,11 @@
 """
 Pixiv API library
 """
-__version__ = '3.5.11'
+__version__ = '3.6.0'
 
 from .papi import PixivAPI
 from .aapi import AppPixivAPI
 from .bapi import ByPassSniApi
 from .utils import PixivError
 
-__all__ = ("PixivAPI", "AppPixivAPI", "ByPassSniApi", "PixivError")
+__all__ = ('PixivAPI', 'AppPixivAPI', 'ByPassSniApi', 'PixivError')

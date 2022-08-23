21d8f02579fe9fbafd95ecfcea2bd846d23422ac
Mantas Mikulėnas <grawity@gmail.com>
2022-08-23 11:04:22 +0300

    mkca.rb: factor out certificate creation

--- security/mkca.rb
+++ security/mkca.rb
@@ -68,6 +68,25 @@ def generate_key(key_type)
     end
 end
 
+def create_certificate(subject, days, priv_key)
+    cert = OpenSSL::X509::Certificate.new
+    cert.version = 2
+    cert.subject = OpenSSL::X509::Name.parse_rfc2253(subject)
+    cert.issuer = cert.subject
+    cert.not_before = Time.now
+    cert.not_after = cert.not_before + (days * 86400)
+    cert.public_key = priv_key
+
+    ef = OpenSSL::X509::ExtensionFactory.new
+    ef.subject_certificate = cert
+    ef.issuer_certificate = cert
+    cert.extensions << ef.create_extension("basicConstraints", "CA:TRUE", true)
+
+    cert.sign(priv_key, OpenSSL::Digest::SHA256.new)
+
+    return cert
+end
+
 def parse_lifetime(string)
     case string
         when /^(\d+)y$/
@@ -122,20 +141,7 @@ rescue => e
     exit 1
 end
 
-cert = OpenSSL::X509::Certificate.new
-cert.version = 2
-cert.subject = OpenSSL::X509::Name.parse_rfc2253(subject)
-cert.issuer = cert.subject
-cert.not_before = Time.now
-cert.not_after = cert.not_before + (days * 86400)
-cert.public_key = priv_key
-
-ef = OpenSSL::X509::ExtensionFactory.new
-ef.subject_certificate = cert
-ef.issuer_certificate = cert
-cert.extensions << ef.create_extension("basicConstraints", "CA:TRUE", true)
-
-cert.sign(priv_key, OpenSSL::Digest::SHA256.new)
+cert = create_certificate(subject, days, priv_key)
 
 if out_cert
     File.open(out_cert, overwrite ? "w" : "wx", 0o644) do |f|

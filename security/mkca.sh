4138a10863772909bac8edd3329087dd948b1544
Mantas Mikulėnas <grawity@gmail.com>
2022-12-14 19:13:20 +0200

    Revert "mkca.sh: include digitalSignature usage"
    Temporarily, for consistency with mkca.py which doesn't set it.
    
    This reverts commit 2c836e7e1a2570711aeb7c77a778692535debd33.

--- security/mkca.sh
+++ security/mkca.sh
@@ -206,7 +206,7 @@ if [[ $tool == openssl ]]; then
 	[dn]
 	[exts]
 	basicConstraints = critical, CA:TRUE
-	keyUsage = critical, digitalSignature, cRLSign, keyCertSign
+	keyUsage = critical, cRLSign, keyCertSign
 	subjectKeyIdentifier = hash
 	EOF
 	# openssl automatically generates a 160-bit serial number
@@ -240,7 +240,6 @@ elif [[ $tool == gnutls ]]; then
 	dn = "$opt_subject"
 	expiration_days = $days
 	ca
-	signing_key
 	cert_signing_key
 	crl_signing_key
 	EOF

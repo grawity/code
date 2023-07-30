44f45af5bceabef0f7532a83c04259b9676e257d
Mantas Mikulėnas <grawity@gmail.com>
2023-07-30 12:58:53 +0300

    remove foo-dl

--- lib/python/pixivpy3/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Pixiv API library
-"""
-
-from .aapi import AppPixivAPI
-from .bapi import ByPassSniApi
-from .utils import PixivError
-
-__all__ = ("AppPixivAPI", "ByPassSniApi", "PixivError")
